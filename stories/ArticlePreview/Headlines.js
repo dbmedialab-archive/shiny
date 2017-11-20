20d85b48def33d1ec238e7c771e554a6ee369fd3
misund <thomas@hemmeligadresse.com>
2017-11-20 15:20:26 +0100

    Refactor ArticlePreview into TrysilPlug

--- stories/ArticlePreview/Headlines.js
+++ stories/ArticlePreview/Headlines.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 import SiteSelection from '../SiteSelection';
-import ArticlePreview from '../../src/molecules/ArticlePreview';
+import TrysilPlug from '../../src/molecules/TrysilPlug';
 
 export default () => (
 	<SiteSelection>
 		<div style={{ margin: '0 auto', maxWidth: '900px' }}>
 			<h1>20.1 - Headlines!</h1>
-			<ArticlePreview
+			<TrysilPlug
 				id={1}
 				link="#"
 				title="Politiet vil fengsle SOS Rasisme-leder i fire uker"
 			/>
-			<ArticlePreview
+			<TrysilPlug
 				id={2}
 				link="#"
 				title="Person funnet død i vannet i Skien - 62-åring savnet i samme område"
