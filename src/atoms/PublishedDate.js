cfb67874a48915a099f9e466622948878c73dd53
Just Thomas Hiorth Misund <thomas@hemmeligadresse.com>
2017-11-21 11:49:53 +0100

    Tweak typography (#26)
    * Tweak typography
    
    * Fix wording in heading story

--- src/atoms/PublishedDate.js
+++ src/atoms/PublishedDate.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 const PublishedDate = styled.span`
 	display: ${props => (props.hide && 'none') || 'inline-block'};
-	color: ${props => props.theme.colors[props.color] || props.theme.colors.grayTintDark};
+	color: ${props => props.theme.colors[props.color] || props.theme.colors.typeLight};
 	font-size: 1.1rem;
 	letter-spacing: 0.1em;
 	text-transform: uppercase;
 `;
 
 PublishedDate.defaultProps = {
-	colors: 'grayTintDark',
+	colors: 'typeLight',
 };
 
 export default PublishedDate;
--- stories/typography/publishedDate.js
+++ stories/typography/publishedDate.js
@@ -2,29 +2,29 @@ import React from 'react';
 
 import LeetTextarea from '../../src/atoms/LeetTextarea';
 
-import { SiteSelection } from '../storybook-components';
 import Heading, { SmallHeading } from '../../src/atoms/Heading';
 import PublishedDate from '../../src/atoms/PublishedDate';
 
 export default () => (
-	<SiteSelection>
-		<section>
-			<Heading>PublishedDate</Heading>
-			<SmallHeading>Preview</SmallHeading>
-			<PublishedDate>Publisert 21. november 2017</PublishedDate>
+	<section>
+		<Heading>PublishedDate</Heading>
+		<SmallHeading>Preview</SmallHeading>
+		<PublishedDate>Publisert 21. november 2017</PublishedDate>
+		<PublishedDate hide>Publisert 21. november 2017</PublishedDate>
+		<PublishedDate color="primary">Publisert 21. november 2017</PublishedDate>
 
-			<SmallHeading>Usage</SmallHeading>
-			<LeetTextarea>{`
+		<SmallHeading>Usage</SmallHeading>
+		<LeetTextarea>{`
 import PublishedDate from '/path/to/shiny/atoms/PublishedDate';
 
 <PublishedDate>Publisert 21. november 2017</PublishedDate>
 <PublishedDate hide>Publisert 21. november 2017</PublishedDate>
-      `}
-			</LeetTextarea>
-			<p>
-				The «hide» prop hides the date.
-				The «color» prop can be any of the color names in the palette. Defaults to «grayTintDark»
-			</p>
-		</section>
-	</SiteSelection>
+<PublishedDate color="primary">Publisert 21. november 2017</PublishedDate>
+   `}
+		</LeetTextarea>
+		<p>
+			The <code>hide</code> prop hides the date.
+			The <code>color</code> prop can be any of the color names in the palette. Defaults to <code>typeLight</code>
+		</p>
+	</section>
 );
