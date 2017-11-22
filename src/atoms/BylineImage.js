32ae9a396726a79800fd14ac987b0bfb56567bb2
thomaslilja <t.lilja83@gmail.com>
2017-11-22 10:27:46 +0100

    Added BylineImage style and story

--- src/atoms/BylineImage.js
+++ src/atoms/BylineImage.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
 const BylineImage = styled.img`
+	width: 6.4rem;
+   height: 6.4rem;
+	border-radius: 50%;
+	display: inline-block;
+	margin: -3.2rem 0.8rem 0 1.5rem;
+	position: relative;
+	border: 3px solid white;
+	box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37);
 `;
 
 export default BylineImage;
