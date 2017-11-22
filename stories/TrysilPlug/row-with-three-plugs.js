c1dc4bdfd85ab6f8a5fdf5486f489d440bc96784
misund <thomas@hemmeligadresse.com>
2017-11-22 16:34:07 +0100

    Improve TrysilPlug story

---
+++ stories/TrysilPlug/row-with-three-plugs.js
@@ -0,0 +1,71 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+import Heading, { HugeHeading } from '../../src/atoms/Heading';
+import { Row, Col } from '../../src/atoms/Grid';
+import TrysilPlug from '../../src/molecules/TrysilPlug';
+
+export default () => (
+	<section>
+		<HugeHeading>TrysilPlug</HugeHeading>
+
+		<p>A row with three TrysilPlugs.</p>
+
+		<Heading>Usage</Heading>
+		<Row>
+			<Col xs>
+				<TrysilPlug
+					title="Hva skal vi plugge i dag, da?"
+					subtitle="Det blir vel en nyhetsartikkel igjen, tenker jeg."
+					image="https://2.dbstatic.no/68712816.jpg?imageId=68712816&x=4.7222222222222&y=28.976572133169&cropw=90.740740740741&croph=60.419235511714&width=980&height=490&compression=60"
+					url="https://example.com"
+					labels={[
+						{
+							text: 'Meninger',
+							color: 'orange',
+						},
+						{
+							text: 'I am label',
+							color: 'blue',
+						},
+					]}
+				/>
+			</Col>
+			<Col xs>
+				<TrysilPlug
+					title="Politiet vil fengsle SOS Rasisme-leder i fire uker"
+					subtitle="Det må meldes i avisa."
+					image="https://2.dbstatic.no/68712816.jpg?imageId=68712816&x=4.7222222222222&y=28.976572133169&cropw=90.740740740741&croph=60.419235511714&width=980&height=490&compression=60"
+					url="https://example.com"
+					labels={[
+						{
+							text: 'Meninger',
+							color: 'orange',
+						},
+						{
+							text: 'I am label',
+							color: 'blue',
+						},
+					]}
+				/>
+			</Col>
+			<Col xs>
+				<TrysilPlug
+					title="Person funnet død i vannet i Skien - 62-åring savnet i samme område"
+					image="https://2.dbstatic.no/68712816.jpg?imageId=68712816&x=4.7222222222222&y=28.976572133169&cropw=90.740740740741&croph=60.419235511714&width=980&height=490&compression=60"
+					url="https://example.com"
+					labels={[
+						{
+							text: 'Meninger',
+							color: 'orange',
+						},
+						{
+							text: 'I am label',
+							color: 'blue',
+						},
+					]}
+				/>
+			</Col>
+		</Row>
+	</section>
+);
