78514d08ba3e3abbd2ea7aec346a41145e4a0b9a
thomaslilja <t.lilja83@gmail.com>
2017-11-22 15:54:40 +0100

    Added BylineName style and story

--- src/atoms/BylineName.js
+++ src/atoms/BylineName.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
 const BylineName = styled.span`
-
+	font-size: 1.6rem;
+	font-weight: 300;
+	padding: 0;
+	text-transform: uppercase;
+	display: inline-block;
+	position: relative;
+	margin-top: 0;
+	top: -1rem;
 `;
 
 export default BylineName;
