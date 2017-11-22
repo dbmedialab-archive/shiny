179fb5815602bf8c21e7ecba7b5ad5e6d55ab6c7
thomaslilja <t.lilja83@gmail.com>
2017-11-22 11:03:28 +0100

    Fixed spacing between images

--- src/atoms/BylineImage.js
+++ src/atoms/BylineImage.js
@@ -8,6 +8,7 @@ const BylineImage = styled.img`
 	margin: -3.2rem 0.8rem 0 1.5rem;
 	position: relative;
 	border: 3px solid white;
+	box-sizing: border-box;
 	box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37);
 `;
 
--- src/atoms/Image.js
+++ src/atoms/Image.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 const Image = styled.img`
-max-width: none;
-width: 100%;
-padding: 0;
-opacity: 1;
+	display: block;
+	max-width: none;
+	width: 100%;
+	padding: 0;
+	opacity: 1;
 `;
 
 export default Image;
