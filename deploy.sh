d8a9a1f1a9b5b9bc050f7d8a4b65d351421cc616
misund <thomas@hemmeligadresse.com>
2017-11-23 11:54:05 +0100

    Revert #36 and #37; add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+npm run clean \
+&& npm run build \
+&& cd storybook-static \
+&& git init \
+&& git checkout -b gh-pages \
+&& git add . \
+&& git commit -m 'History will be kind to me, for I intend to write it' \
+&& git push -f git@github.com:dbmedialab/shiny.git gh-pages
