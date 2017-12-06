b7bc54ded97389c8bf253816c7378ee5c0ca8c1d
misund <thomas@hemmeligadresse.com>
2017-12-06 18:03:27 +0100

    Produce svgs in dice

--- stories/typography/dice.js
+++ stories/typography/dice.js
@@ -9,25 +9,29 @@ export default () => (
 	<section>
 		<Heading>Dice</Heading>
 
-		<Dice value="1" />
-		<Dice value="2" />
-		<Dice value="3" />
-		<Dice value="4" />
-		<Dice value="5" />
-		<Dice value="6" />
+		<Dice value={1} />
+		<Dice value={2} />
+		<Dice value={3} />
+		<Dice value={4} />
+		<Dice value={5} />
+		<Dice value={6} />
 
-		<Dice value="1" size={{ width: '200px', height: '200px' }} />
+		<Dice value={1} size="20rem" />
+		<Dice value={2} size="20rem" color="yellowLighter" pipColor="purpleDark" />
+		<Dice value={3} size="20rem" color="secondary" />
+		<Dice value={4} size="20rem" color="teal" />
+
 
 		<Heading>Usage</Heading>
 		<LeetTextarea>{`
-                import Dice from '/path/to/shiny/atoms/Dice';
+ import Dice from '/path/to/shiny/atoms/Dice';
 
-                <p>Og det faktum at den nå eksisterer som en kinofilm med et 
-                budsjett på 50 millioner dollar, er ugjendrivelig bevis på at vi 
-                lever i en tid der Hollywood langsomt og udramatisk er i ferd 
-                med å kveles av kommersialismens kalde dødsgrep.</p>
+ <p>Og det faktum at den nå eksisterer som en kinofilm med et
+ budsjett på 50 millioner dollar, er ugjendrivelig bevis på at vi
+ lever i en tid der Hollywood langsomt og udramatisk er i ferd
+ med å kveles av kommersialismens kalde dødsgrep.</p>
 
-                <Dice value="1" />
+ <Dice value="1" />
             `}
 		</LeetTextarea>
 	</section>
