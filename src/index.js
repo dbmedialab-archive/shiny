73775a2489d9cf83e88899b64f773ea8a90c2d75
Just Thomas Hiorth Misund <thomas@hemmeligadresse.com>
2017-12-12 20:48:26 +0100

    Add/ts decl (#65)
    * Export all existing atoms and molecules as submodules
    
    * Add type definitions for some more modules

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// Atoms
+export { Article                 } from './atoms/Article';
+export { BlockLink               } from './atoms/BlockLink';
+export { BlockLinkWithBackground } from './atoms/BlockLinkWithBackground';
+export { BlockQuote              } from './atoms/BlockQuote';
+export { BodyText                } from './atoms/BodyText';
+export { Button                  } from './atoms/Button';
+export { BylineImage             } from './atoms/BylineImage';
+export { BylineName              } from './atoms/BylineName';
+export { Caption                 } from './atoms/Caption';
+export { Cite                    } from './atoms/Cite';
+export { Col                     } from './atoms/Col';
+export { ColorBox                } from './atoms/ColorBox';
+export { DontPushTheAdBoundaries } from './atoms/DontPushTheAdBoundaries';
+export { Dropdown                } from './atoms/Dropdown';
+export { Figure                  } from './atoms/Figure';
+export { GlobalStyle             } from './atoms/GlobalStyle';
+export { Grid                    } from './atoms/Grid';
+export { Header                  } from './atoms/Header';
+export { Image                   } from './atoms/Image';
+export { Kicker                  } from './atoms/Kicker';
+export { Label                   } from './atoms/Label';
+export { LeetTextarea            } from './atoms/LeetTextarea';
+export { Link                    } from './atoms/Link';
+export { ListItem                } from './atoms/ListItem';
+export { LunchKicker             } from './atoms/LunchKicker';
+export { NativeAdKicker          } from './atoms/NativeAdKicker';
+export { Pips                    } from './atoms/Pips';
+export { PublishedDate           } from './atoms/PublishedDate';
+export { Quote                   } from './atoms/Quote';
+export { Row                     } from './atoms/Row';
+export { Standfirst              } from './atoms/Standfirst';
+export { ThemeName               } from './atoms/ThemeName';
+
+// Molecules
+export { HorizontalLinkBar       } from './molecules/HorizontalLinkBar/HorizontalLinkBar';
+export { Dice                    } from './molecules/Dice';
+export { FloatImage              } from './molecules/FloatImage';
+export { LunchButton             } from './molecules/LunchButton';
+export { TrysilPlug              } from './molecules/TrysilPlug';
