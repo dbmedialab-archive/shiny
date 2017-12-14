f9471f13f6be35e227de2a4f5699440085fd7e4f
misund <thomas@hemmeligadresse.com>
2017-12-14 16:53:22 +0100

    Update bylines

--- src/atoms/BylineName.js
+++ src/atoms/BylineName.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 const BylineName = styled.span`
-	font-size: 1.6rem;
+	font-size: ${props => props.theme.variables.headingSmallSize};
+	line-height: ${props => props.theme.variables.headingSmallLineHeight};
 	font-weight: 300;
 	padding: 0;
 	text-transform: uppercase;
 	display: inline-block;
 	position: relative;
 	margin-top: 0;
-	top: -1rem;
+	top: calc(-1/2 * ${props => props.theme.variables.verticalBase});
 `;
 
 export { BylineName };
--- stories/editorialMarking/bylineImage.js
+++ stories/editorialMarking/bylineImage.js
@@ -15,7 +15,9 @@ export default () => (
 		<Heading>Demo</Heading>
 		<DemoContainer>
 			<Image src="https://styleguide.dagbladet.no/assets/test/large-q60.jpg" />
-			<BylineImage src="https://styleguide.dagbladet.no/assets/bylines/msi.png" />
+			<BylineImage
+				src="https://dbstatic.no/60384708.jpg?imageId=60384708&x=0&y=0&cropw=100.00&croph=100.00&width=496&height=496"
+			/>
 		</DemoContainer>
 
 		<Heading>Usage</Heading>
@@ -27,7 +29,9 @@ import {
 } from '@aller/shiny';
 
 <Image src="https://styleguide.dagbladet.no/assets/test/large-q60.jpg" />
-<BylineImage src="https://styleguide.dagbladet.no/assets/bylines/msi.png" />
+<BylineImage
+	src="https://dbstatic.no/60384708.jpg?imageId=60384708&x=0&y=0&cropw=100.00&croph=100.00&width=496&height=496"
+/>
 			`}
 		</LeetTextarea>
 	</section>
