e7e126e2dbafaaa7cd9976930ed0178ff55162d0
Just Thomas Hiorth Misund <thomas@hemmeligadresse.com>
2018-01-12 14:14:34 +0100

    refactor: change font definitions (#123)
    * refactor: change font definitions
    
    * fix: Clean up themes; reduce visual changes

--- src/themes/dinside/index.js
+++ src/themes/dinside/index.js
@@ -1,15 +1,24 @@
 import colors from './colors';
 
+const variables = {
+	mainFont: "'Open Sans', helvetica, arial, sans-serif",
+	headingsFont: "'Open Sans', helvetica, arial, sans-serif",
+};
+
 const global = `
 	a {
 		color: #333;
 	}
+
+	font-family: ${variables.mainFont};
+	line-height: ${variables.verticalBase};
 `;
 
 const theme = {
 	name: 'DinSide',
 	global,
 	colors,
+	variables,
 };
 
 export default theme;
--- src/themes/sol/index.js
+++ src/themes/sol/index.js
@@ -2,6 +2,7 @@ import colors from './colors';
 import variables from './variables';
 
 const global = `
+	@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700,800');
 `;
 
 export default {
