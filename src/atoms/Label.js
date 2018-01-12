c7e9178e93811f6a78d508b66428bcbafd6ab7a8
misund <thomas@hemmeligadresse.com>
2018-01-12 15:35:38 +0100

    fix(Label): Tweak labels; add textColor prop

--- src/atoms/Label.js
+++ src/atoms/Label.js
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
 
 const Label = styled.span`
-	background-color: ${props => props.theme.colors[props.color] || props.theme.colors.primary};
-	color: #333;
+	background-color: ${props => props.theme.colors[props.color] || props.theme.colors[props.theme.colors.skinColors.splashBackground]};
+	color: ${props => props.theme.colors[props.textColor] || props.theme.colors[props.theme.colors.skinColors.splashText]};
 	display: inline-block;
-	font-family: 'Roboto', 'Helvetica', Helvetica, Arial, sans-serif;
-	/* font-size: 0.6875rem; */
+	font-family: ${props => props.theme.variables.headingsFont};
 	font-size: 1.1rem;
 	font-weight: 300;
-	letter-spacing: 1px;
+	letter-spacing: .1rem;
 	line-height: 1;
 	margin-bottom: auto;
-	margin-right: 1px;
+	margin-right: .1rem;
 	padding: 0.25rem 0.5rem 0.25rem;
 	position: relative;
 	text-align: center;
