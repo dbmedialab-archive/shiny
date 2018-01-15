79d975855b71c679a869353940635ceaffa17687
misund <thomas@hemmeligadresse.com>
2018-01-15 14:58:57 +0100

    Update documentation

--- src/atoms/ThemeSelector.js
+++ src/atoms/ThemeSelector.js
@@ -7,7 +7,6 @@ import { themes } from '../themes';
 
 const isClient = (typeof window !== 'undefined');
 
-// @TODO Alle fontene inkluderes her - må fikses før vi går live!
 injectGlobal`
   /* Font reset: 1rem = 10px */
   html {
@@ -18,6 +17,15 @@ injectGlobal`
   }
 `;
 
+/**
+ * An extension of styled-component's ThemeProvider.
+ *
+ * Why do we need this, you might ask? In addition to what we
+ * get from styled-components, this ThemeProvider:
+ * - allows us to pass in the name of the theme instead of the theme itself
+ * - deep merges our theme with the default theme
+ * - injects our theme's global styles
+ */
 const ThemeSelector = ({ children, themeSlug }) => {
 	const hasThemeStoredInBrowser = (isClient && window.localStorage && window.localStorage.getItem('theme'));
 	const themeSlugFromLocalStorage = hasThemeStoredInBrowser ? window.localStorage.getItem('theme') : 'defaultTheme';
@@ -26,14 +34,20 @@ const ThemeSelector = ({ children, themeSlug }) => {
 	// Failing that, try localStorage
 	const themeName = themeSlug ? themeSlug : themeSlugFromLocalStorage;
 
+	// Deep merge the chosen theme with our default theme.
+	// This way, we can always assume that every property
+	// in the default theme will have a value
+	// no matter which theme we are using
 	const theme = merge(themes.defaultTheme, themes[themeName]);
+
 	// console.log(`Switching to the ${themeName} theme.`);
 	// console.log('new theme', theme.name);
 
+	// Inject our theme's global styles
 	injectGlobal`${theme.global}`;
 
 	/**
-	 * TODO: Make that div a React.Fragment
+	 * TODO: Make that inner div a React.Fragment
 	 *
 	 * aurora-frontend is still not on Fiber. ThemeProvider expects an only child.
 	 */
