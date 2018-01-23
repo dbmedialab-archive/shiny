a4eeded6c6d132fc6393e54260c6267dc047db94
misund <thomas@hemmeligadresse.com>
2018-01-23 11:27:30 +0100

    Fix line-height in ScrollArrow as well

--- src/atoms/ScrollArrow.js
+++ src/atoms/ScrollArrow.js
@@ -4,14 +4,13 @@ import { css } from 'styled-components';
 import { LinkBarLinkBase } from '../..';
 
 const Arrow = LinkBarLinkBase.withComponent('div').extend`
-	line-height: 0;
+	position: absolute;
+	top: 0;
+	${props => (props.pointsTo === 'left' ? css`left: 0` : css`right: 0;`)};
 	font-weight: 800;
 	cursor: pointer;
 	z-index: 1;
-	position: absolute;
-	top: 0;
 	background: ${props => props.background};
-	${props => (props.pointsTo === 'left' ? css`left: 0` : css`right: 0;`)};
 `;
 
 export const LeftScrollArrow = props => <Arrow {...props}>&#8826;</Arrow>;
