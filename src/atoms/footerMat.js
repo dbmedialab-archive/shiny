8a7d9f3fd2354cd4753db98c8a998f9eef5e49ba
EvoX0 <rodionantonichev@gmail.com>
2018-01-24 11:54:48 +0200

    footer >

--- src/atoms/footerMat.js
+++ src/atoms/footerMat.js
@@ -1,16 +1,24 @@
 import styled from 'styled-components';
 
 const FooterMat = styled.div`
+	width: 980px;
 	background: #515151;
-	padding: 0;
-	position: relative;
+	padding: 50px;
 	box-sizing: border-box;
 	color: #fff;
 	font-family: 'Cabin', sans-serif;
 	border-radius: .3rem;
 	overflow: hidden;
-	margin-bottom: ${props => props.theme.variables.verticalBase};
 	font-size: 14px;
+	& p {
+		margin: 0;
+		width: 185px;
+		margin-left: 20px;
+	}
+	&.col {
+		color: red;
+	}
+	
 `;
 
 
