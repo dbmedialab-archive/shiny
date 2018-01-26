34288a2413cbab81cddf9c3e8d8992333b39844b
Fredrik Pettersen <fredrik.fumler@gmail.com>
2018-01-26 16:23:54 +0100

    remove margin left and right from article component

--- src/atoms/Article.js
+++ src/atoms/Article.js
@@ -1,8 +1,6 @@
 import Styled from 'styled-components';
 
 const Article = Styled.article`
-	margin-left: calc(${props => props.theme.variables.horizontalBase} / 2);
-	margin-right: calc(${props => props.theme.variables.horizontalBase} / 2);
 	margin-bottom: ${props => props.theme.variables.verticalBase};
 `;
 
