607231d3b472bb50060c206d77063079d876a752
EvoX0 <rodionantonichev@gmail.com>
2018-01-30 18:09:50 +0200

    + social

--- src/atoms/MainRecipe/Wrapper.js
+++ src/atoms/MainRecipe/Wrapper.js
@@ -3,6 +3,7 @@ import Styled from 'styled-components';
 const Wrapper = Styled.div`
 	width: 1440px;
 	font-family: Cabin;
+	margin: 0 auto;
 `;
 
 export { Wrapper };
--- src/atoms/MatHeader/Logo.js
+++ src/atoms/MatHeader/Logo.js
@@ -3,7 +3,8 @@ import Styled from 'styled-components';
 const Logo = Styled.svg`
 	position: absolute;
 	left: 90px;
-	width: 125px
+	width: 125px;
+	z-index: 1;
 `;
 
 export { Logo };
