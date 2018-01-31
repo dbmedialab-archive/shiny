2d2e13cc835e3c5ef792b85d3baa446fbd30b4a0
Jon Inge Moe <jim@sol.no>
2018-01-31 09:04:52 +0100

    yepp

--- stories/link-bars/ScrollArrowsGradientBackgroundStory.js
+++ stories/link-bars/ScrollArrowsGradientBackgroundStory.js
@@ -27,7 +27,7 @@ const ScrollArrowsGradientBackground = () => (
 				background={`linear-gradient(0deg, ${colors.grayTint}, ${colors.white} .1rem)`}
 				shouldAdjustForNestedPadding
 				drawRightArrowInitially
-				arrowGradientBackground={`${colors.white}`}
+				arrowGradientBackground={colors.white}
 				arrowGradientHeight="97%"
 			>
 				<LinkBarLink linkText="One long link" url="https://example.com" isActive />
@@ -61,8 +61,11 @@ import {
 } from '@aller/shiny';
 
 <ScrollArrowsLinkBar
-	background={colors.white}
+	background={\`linear-gradient(0deg, \${colors.grayTint}, \${colors.white} .1rem)\`}
+	shouldAdjustForNestedPadding
 	drawRightArrowInitially
+	arrowGradientBackground={colors.white}
+	arrowGradientHeight="97%"
 >
 	<LinkBarLink linkText="One long link" url="https://example.com" isActive />
 	<LinkBarLink linkText="Two long link" url="https://example.com" />
