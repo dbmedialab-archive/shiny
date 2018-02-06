5f74d8a7267f13c99d02db38da04e1076ca00a45
tcat <trustut@gmail.com>
2018-02-06 11:44:54 +0200

    clean up

--- src/atoms/UnderlinedHeading.js
+++ src/atoms/UnderlinedHeading.js
@@ -63,57 +63,6 @@ const UnderlinedHugeHeading = ({children}) => (
 	</CenteredContent>
 );
 
-/*
-const UnderlinedSmallHeading = Heading.extend`
-	font-size: ${props => props.theme.variables.headingSmallSize};
-	line-height: ${props => props.theme.variables.headingSmallLineHeight};
-
-	@media screen and (min-width: ${props => props.theme.variables.largeWidth}) {
-		font-size: ${props => props.theme.variables.headingRegularSize};
-		line-height: ${props => props.theme.variables.headingRegularLineHeight};
-	}
-`;
-
-const UnderlinedMediumHeading = Heading.extend`
-	font-size: ${props => props.theme.variables.headingMediumSize};
-	line-height: ${props => props.theme.variables.headingMediumLineHeight};
-
-	@media screen and (min-width: ${props => props.theme.variables.largeWidth}) {
-		font-size: ${props => props.theme.variables.headingRegularSize};
-		line-height: ${props => props.theme.variables.headingRegularLineHeight};
-	}
-`;
-
-const UnderlinedLargeHeading = Heading.extend`
-	font-size: ${props => props.theme.variables.headingRegularSize};
-	line-height: ${props => props.theme.variables.headingRegularLineHeight};
-
-	@media screen and (min-width: ${props => props.theme.variables.largeWidth}) {
-		font-size: ${props => props.theme.variables.headingLargeSize};
-		line-height: ${props => props.theme.variables.headingLargeLineHeight};
-	}
-`;
-
-const XLargeHeading = Heading.extend`
-	font-size: ${props => props.theme.variables.headingRegularSize};
-	line-height: ${props => props.theme.variables.headingRegularLineHeight};
-
-	@media screen and (min-width: ${props => props.theme.variables.largeWidth}) {
-		font-size: ${props => props.theme.variables.headingXlargeSize};
-		line-height: ${props => props.theme.variables.headingXlargeLineHeight};
-	}
-`;
-
-const HugeHeading = Heading.extend`
-	font-size: ${props => props.theme.variables.headingLargeSize};
-	line-height: ${props => props.theme.variables.headingLargeLineHeight};
-
-	@media screen and (min-width: ${props => props.theme.variables.largeWidth}) {
-		font-size: ${props => props.theme.variables.headingHugeSize};
-		line-height: ${props => props.theme.variables.headingHugeLineHeight};
-	}
-`;
-*/
 export {
 	UnderlinedLargeHeading as UnderlinedHeading,
 	UnderlinedLargeHeading,
