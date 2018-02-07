9d0a14ae10cdb812e68a641a1eebf322a90b1e2e
EvoX0 <rodionantonichev@gmail.com>
2018-02-07 14:11:19 +0200

    init Front Heading

---
+++ src/atoms/FrontHeading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import styled from 'styled-components';
+import {
+	SmallHeading,
+	MediumHeading,
+	LargeHeading,
+	XLargeHeading,
+	HugeHeading,
+} from './Heading';
+
---
+++ stories/typography/front-heading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { DemoContainer } from '../storybook-components';
+import { Row } from '../../src/atoms/Row';
+import { Col } from '../../src/atoms/Col';
+import { Code } from '../../src/atoms/Code';
+
+export default () => (
+	<h1>Test</h1>
+);
