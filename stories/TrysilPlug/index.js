9c00b5398aeedf1f3fe740818130a3c847b74cda
misund <thomas@hemmeligadresse.com>
2018-02-13 20:32:37 +0100

    Add an ALLCAPS prop to TrysilPlug

--- stories/TrysilPlug/index.js
+++ stories/TrysilPlug/index.js
@@ -3,10 +3,12 @@ import { storiesOf } from '@storybook/react';
 import basicIssue from './basic-issue';
 import rowWithThreePlugs from './row-with-three-plugs';
 import kicker from './kicker';
+import allCaps from './all-caps';
 
 export default () => {
 	storiesOf('TrysilPlug', module)
 		.add('A typical plug', basicIssue)
 		.add('With Kicker', kicker)
+		.add('All caps', allCaps)
 		.add('Row with three plugs', rowWithThreePlugs);
 };
