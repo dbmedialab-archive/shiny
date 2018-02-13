9435982a963262ed9320ebdaa7fba072f1e1e99a
ivlu <ivlu@ciklum.com>
2018-02-13 11:46:46 +0200

    Added stars rating atom

--- src/molecules/MainRecipe/DetailsSection.js
+++ src/molecules/MainRecipe/DetailsSection.js
@@ -8,7 +8,7 @@ import { FrontSmallHeading } from '../../atoms/FrontHeading';
 
 import { IconBar } from '../IconBar';
 // TODO uncomment after rating will be added
-// import { StarsRating } from '../../atoms/StarsRating';
+import { StarsRating } from '../../atoms/StarsRating';
 import { BylineWithTwoLines } from '../../atoms/BylineWithTwoLines';
 
 const DetailsSectionInfo = Styled.div`
@@ -53,7 +53,7 @@ const DetailsSection  = ({
 			</Col>
 			<Col xs={12} md={12} lg={6}>
 				<FrontSmallHeading>Vurdering</FrontSmallHeading>
-				{/* <StarsRating count={5} size={45} value={rating} /> */}
+				<StarsRating count={5} size={45} value={rating} />
 			</Col>
 		</Row>
 	</DetailsSectionWrapper>
