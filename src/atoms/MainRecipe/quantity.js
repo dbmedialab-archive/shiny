3fd026180af39490dbacd2db02b885bedfd819b2
EvoX0 <rodionantonichev@gmail.com>
2018-02-13 14:22:34 +0200

    props added

--- src/atoms/MainRecipe/quantity.js
+++ src/atoms/MainRecipe/quantity.js
@@ -6,14 +6,12 @@ import { Row } from '../../atoms/Row';
 export const Quantity = props => (
 	<section>
 		<Row>
-			<Col xs={12} md={1}>
-				<span>
-					1 ss
-				</span>
+			<Col xs={3} md={2} lg={1}>
+				<span> {props.amount} </span>
 			</Col>
-			<Col xs={12} md={3}>
+			<Col xs={9} md={10} lg={11}>
 				<span>
-					margarin <i>(eller olje til steking)</i>
+					{props.name}
 				</span>
 			</Col>
 		</Row>
