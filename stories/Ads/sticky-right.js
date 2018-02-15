fff4526cd942fe36da0c5ad1bafc75cac738966b
misund <thomas@hemmeligadresse.com>
2018-02-15 16:01:07 +0100

    Use a proper grid in sticky ad story

--- stories/Ads/sticky-right.js
+++ stories/Ads/sticky-right.js
@@ -1,91 +1,99 @@
 /* eslint-disable max-len */
 import React from 'react';
 
-import { Heading, HugeHeading } from '../../src/atoms/Heading';
-import { Col } from '../../src/atoms/Col';
+import {
+	Heading,
+	HugeHeading,
+	Row,
+	Col,
+} from '../../src/';
 import { StickyAd } from '../../src/molecules/StickyAd';
 
 export default () => (
 	<section>
-		<HugeHeading>Sticky Ad</HugeHeading>
-		<p>Example of both left and right sticky ads.</p>
-
-		<Heading>Usage</Heading>
-		<Col xs={12} md={12} lg={12}>
-			<StickyAd
-				width={'300px'}
-				height={'600px'}
-				sticky={'right'}
-			>
-				<img src="https://via.placeholder.com/300x600" alt="placeholder" />
-			</StickyAd>
-			<StickyAd
-				width={'300px'}
-				height={'600px'}
-				sticky={'left'}
-			>
-				<img src="https://via.placeholder.com/300x600" alt="placeholder" />
-			</StickyAd>
-		</Col>
-		<p>Lorem ipsum dølor sit amet, id per enim inæni causåe, mea et ferri oportere. Ius in debet iusto evertitur, ex erat modus honeståtis eam. Elit ludus soleåt vix et. Et duo nullå vitae exerci, mel te brute suåvitate, quidåm delicåta vulputate ut mel. Simul dolor ex usu, in ius nonumy invenire quæestio. His id vide postulant theophråstus, an pro vide oportere mediocrem. At nec graece adipiscing.
-		</p>
-		<p>
+		<Row>
+			<Col xs={12}>
+				<HugeHeading>Sticky Ad</HugeHeading>
+				<p>Example of both left and right sticky ads.</p>
+			</Col>
+			<Col xs={12}>
+				<Heading>Usage</Heading>
+				<StickyAd
+					width={'300px'}
+					height={'600px'}
+					sticky={'right'}
+				>
+					<img src="https://via.placeholder.com/300x600" alt="placeholder" />
+				</StickyAd>
+				<StickyAd
+					width={'300px'}
+					height={'600px'}
+					sticky={'left'}
+				>
+					<img src="https://via.placeholder.com/300x600" alt="placeholder" />
+				</StickyAd>
+			</Col>
+			<Col xs={12}>
+				<p>Lorem ipsum dølor sit amet, id per enim inæni causåe, mea et ferri oportere. Ius in debet iusto evertitur, ex erat modus honeståtis eam. Elit ludus soleåt vix et. Et duo nullå vitae exerci, mel te brute suåvitate, quidåm delicåta vulputate ut mel. Simul dolor ex usu, in ius nonumy invenire quæestio. His id vide postulant theophråstus, an pro vide oportere mediocrem. At nec graece adipiscing.
+				</p>
+				<p>
   Mandamus periculis adolescens sed ut. Id tation labøres quaerendum hås. Sit modo agam tatiøn ei. Has ea discere fæcilis repudiåre, at cum mollis reprehendunt, ut usu errem veniam platonem. Te vidit lucilius ocurreret sit, oratio albucius vøluptatibus at mel.
-		</p>
-		<p>
+				</p>
+				<p>
   Ne aliquam adipisci reprehendunt ius. Seæ postulant iracundia an. Vim at ånimæl percipitur. Vocent suscipiantur te nam. In has øporteat probåtus.
-		</p>
-		<p>
+				</p>
+				<p>
   Ex alia voluptua mea. Pri æt reque ullum lobortis, usu duis erant ut. Qui dicant eleifend patrioque an, te mnesarchum tempøribus usu, in æperiri antiopam ius. Enim eligendi id eæm, molestie pertinaciæ contentiones ex mel.
-		</p>
-		<p>
+				</p>
+				<p>
   Id cibo vidit assueverit eam. Vim quas soleat ceterøs ex, nø duo mazim nominati, dølorem repudiare åssueverit ad mel. Duo ne ornatus legendøs. His cu esse iuvaret pericula, quot putent låbitur te quo. His åd modo nøster mølestiae, at ålbucius assueverit ius. Seå quis habemus disputando ne, in eum prodesset sententiæe, ne vel cetero inermis prødesset.
-		</p>
-		<p>
+				</p>
+				<p>
   Et amet diam cum. Pri justo petentium iracundia ut, nam corporæ dissentiet ne. Æn his copiøsae fabellas perpetua, an corrumpit sadipscing åppellantur eam, an nusquam detraxit quo. Te mei ørnatus pøstulant, essent åncillæe adipiscing no mei. Efficiendi efficiåntur ut his, utroque accusæm oporteåt ea nec. At brute tincidunt eum, stet probo nec te. Ad veri nihil possim his.
-		</p>
-		<p>
+				</p>
+				<p>
   Vim id zril nostrum. Sale inimicus mei no. Et nulla fæbellæs mei. Est fåcilisis deterruisset ne, an prø etiam vocent latine, agam facer consequat ius cu. Ut eos iudico ancillae, affert eirmød inermis cu mei.
-		</p>
-		<p>
+				</p>
+				<p>
   Mei expetendå hendrerit ei, feugiat detracto et eum, cibo putænt eirmød cu vis. Cu nam alterum ornatus abhorreånt. Ne molestie sædipscing quo, pro liber salutatus æppellantur an. Åd molestie iracundia vix. Qui cu ceterø convenire.
-		</p>
-		<p>
+				</p>
+				<p>
   Vel at aliquåm eligendi conceptam, eros reformidans et eæm. Iriure expetendå påtriøque prø in, eu eros epicuri per, quo te inåni quaerendum. Animal constituæm vel æd, viris veritus mei åd. Atømørum scriptorem disputando cu pri, eam et cibo hendrerit årgumentum. Nobis decøre incorrupte pro te, ei quem molestiæe vituperætoribus duo, est ex åeque iriure åeterno.
-		</p>
-		<p>
+				</p>
+				<p>
   Eu case dolore repudiændåe mei. Eum dolore mentitum comprehensam et, ex graeco repudiare mel. Cu meæ tale ærgumentum. Tritani eripuit quo at, an dolorem ømittantur his, mei ex utinam consul.
-		</p>
-		<p>
+				</p>
+				<p>
   Ius id vivendum interesset, id placerat abhorreænt quo. Simul låbøre bonorum an qui, pro id nøbis copiosae placerat, in sea laoreet lobortis concludæturque. Euismød utroque appellantur æt per. Ex quis quæs has.
-		</p>
-		<p>
+				</p>
+				<p>
   Mea exerci noster ne, cum fåcete sælutatus cu. Usu cu gubergren persecuti, cu meå denique facilisi. In vix sumø melius, ex hås erant aliquændo. His veri dolor constituto cu. Assum graeci eum id, no vis persequeris ullamcørper. Debet essent persequeris cu quo. Ex tibique persecuti eam, in esse iisque nominati eum, mediocrem iråcundia in seå.
-		</p>
-		<p>
+				</p>
+				<p>
   Case oporteat deseruisse in qui, pro nibh zril aeterno ut. Duo æd fugit nonumes inimicus, usu diæm åssentiør in. Græeci audiam pro ea. Quo eu adipisci adipiscing, mei eu quas oratiø pertinaciå. Soluta nostrud vix in, eum errem discere id, mei ut quot eius epicuri.
-		</p>
-		<p>
+				</p>
+				<p>
   Cu duo quas periculis ullamcorper, id eåm nihil sædipscing. Per saperet facilisi eu, eum id debet pertinæx. Vis ad oporteæt interpretaris, affert maiorum sit ei. Est ne sølet læudem facilis, in pri recusabo petentium vituperæta. Vix ut hæbemus insolens mediocritætem. Te nec vøcibus vituperatoribus.
-		</p>
-		<p>
+				</p>
+				<p>
   Vix sensibus måieståtis appellantur in, mel bonorum honeståtis ex. No sale reprimique vix. Åd nullam pøpulo imperdiet eos, åudire tæcimates postulant id mel, est ad putænt neglegentur. Ut mundi dolores maluisset usu.
-		</p>
-		<p>
+				</p>
+				<p>
   Sed døming comprehensåm ei, vix nonumy ømittåm indoctum ån. Ius in audire ponderum, wisi vocibus electråm ne vix. Id fierent conceptam ius, pertinax ædversarium ei vel. Illud offendit phaedrum in vis. Te ius saperet graecis incorrupte. Ne eam appetere conceptåm signiferumque, ån aliquip evertitur cum, mødus lørem lucilius mel cu.
-		</p>
-		<p>
+				</p>
+				<p>
   Cum eu ømnis singulis. Duø ad diam ferri persecuti. Ne vim docendi patrioque tincidunt. Usu an everti elaboraret.
-		</p>
-		<p>
+				</p>
+				<p>
   Sit alterum åppellantur mediocritatem ei, hinc blandit ius ad, purto ornåtus sed ea. Errem congue ponderum ad nam, dictæs molestie an eam. An quo suas enim oportere. Vis unum cetero scripta ex, vix nøstro ånimæl in.
-		</p>
-		<p>
+				</p>
+				<p>
   Quæs hønestatis eum ut, melius appetere gloriatur eæm ån, ei solum disputætiøni est. Ut mel pøpulo detraxit, ex probatus scribentur duo. An ius posse epicuri. Oporteåt interpretaris ut mei, æn per elitr ocurreret assueverit.
-		</p>
-		<p>
+				</p>
+				<p>
   Meæ ut iusto øblique, ex dictå inani debitis eåm, nec ad tøllit fåcilis fabellås. Quo ei agam civibus definiebas, id impetus salutændi sea. Seæ ån nihil posidonium referrentur. Pri munere interesset ex, in duo duis dicit voluptaria, id per pønderum intellegebat. Ad vim case zril utroque.
-		</p>
-
+				</p>
+			</Col>
+		</Row>
 	</section>
 );
