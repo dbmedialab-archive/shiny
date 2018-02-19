51c3702afbad46de779a012c586e1cadc11d3787
misund <thomas@hemmeligadresse.com>
2018-02-19 22:02:35 +0100

    Tweak Mat typography

--- src/atoms/MainRecipe/Description.js
+++ src/atoms/MainRecipe/Description.js
@@ -7,12 +7,14 @@ const Description = styled(Col)`
 	height: 100%;
 	display: flex;
 	align-items: center;
-	font-size: 2.0rem;
-	line-height: 2.4rem;
-	font-weight: 500;
+	font-size:   ${props => props.theme.variables.headingRegularSize};
+	line-height: ${props => props.theme.variables.headingRegularLineHeight};
+	font-weight: 600;
+	letter-spacing: .05rem;
 	text-align: center;
 	color: ${props => props.theme.colors[props.textColor]};
 	background-color: ${props => props.theme.colors[props.bgColor]};
+
 	@media screen and (min-width: ${props => props.theme.flexboxgrid.breakpoints.sm}em) {
 		padding: 2rem 10rem;
  	}
