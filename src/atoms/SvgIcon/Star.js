681bef59bdf6f044d09b68cc22f08097fffe8ee6
ivlu <ivlu@ciklum.com>
2018-02-23 14:25:36 +0200

    Made refactoring after code review

--- src/atoms/SvgIcon/Star.js
+++ src/atoms/SvgIcon/Star.js
@@ -5,7 +5,7 @@ const Star = props => (
 	<svg width="100%" height="100%" viewBox="0 0 31 29">
 		{/* eslint-disable max-len */}
 		<g id="Restaurants" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" >
-			<g id="Desktop" transform="translate(-755.000000, -541.000000)" fill={props.color}>
+			<g id="Desktop" transform="translate(-755.000000, -541.000000)" fill={props.color || '#333333'}>
 				<g id="recipe-1-copy-5" transform="translate(567.000000, 230.000000)">
 					<g id="stars-copy" transform="translate(52.000000, 311.000000)">
 						<path d="M151.706522,0 L156.292349,9.61545136 L166.706522,11.0770118 L159.126389,18.4812498 L160.976986,29 L151.706522,23.9606213 L142.436057,29 L144.286655,18.4812498 L136.706522,11.0770118 L147.120694,9.61545136 L151.706522,0 Z" id="star-2-icon-copy-12" />
