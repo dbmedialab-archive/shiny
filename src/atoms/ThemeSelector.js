2c384fcc47dff110f41ce21a43a7787ddc528904
Fredrik Pettersen <fredrik.fumler@gmail.com>
2018-02-26 14:06:40 +0100

    fix indent

--- src/atoms/ThemeSelector.js
+++ src/atoms/ThemeSelector.js
@@ -8,17 +8,17 @@ import { themes } from '../themes';
 const isClient = (typeof window !== 'undefined');
 
 injectGlobal`
-  /* Font reset: 1rem = 10px */
-  html {
-    font-size: 62.5%;
-  }
-  body {
-    font-size: 1.6rem;
-  }
+	/* Font reset: 1rem = 10px */
+	html {
+		font-size: 62.5%;
+	}
+	body {
+		font-size: 1.6rem;
+	}
 	/* HTML5 display-role reset for older browsers */
-	article, aside, details, figcaption, figure, 
+	article, aside, details, figcaption, figure,
 	footer, header, hgroup, menu, nav, section, main {
-    display: block;
+    	display: block;
 	}
 `;
 
