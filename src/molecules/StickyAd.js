e7ac6a17ea1195d6ee3bb3ec8e6c891f242f8c89
Fredrik Pettersen <fredrik.fumler@gmail.com>
2018-02-28 23:52:52 +0100

    fix indent

--- src/molecules/StickyAd.js
+++ src/molecules/StickyAd.js
@@ -6,8 +6,8 @@ import Sticker from 'react-stickyfill';
 import { AdWrapper } from '../atoms/AdWrapper';
 
 const StyledAdWrapper = AdWrapper.extend`
-  position: sticky;
-  top: 0;
+	position: sticky;
+	top: 0;
 	margin-left: 0;
 	margin-right: 0;
 `;
