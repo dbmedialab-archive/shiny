d94325952443b3516e07d201f3b86e06823e8c51
ivlu <ivlu@ciklum.com>
2018-03-13 19:36:00 +0200

    implemented tags bar

--- src/molecules/TagSection.js
+++ src/molecules/TagSection.js
@@ -1,24 +1,79 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Slider from 'react-slick';
+import { FontIcon } from '../..';
 import { Tag } from '../atoms/Tag';
 
 const TagItemWrapper = styled.span`
-	margin-right: ${props => `calc(3 * ${props.theme.variables.horizontalBase})`};
+	margin-right: ${props => props.theme.variables.horizontalBase};
 	float: left;
 	line-height: ${props => `calc(2 * ${props.theme.variables.headingSmallLineHeight})`};
+	border: 1px solid grey;
+	text-align: center;
+	border-radius: 5px;
+	white-space: nowrap;
+	font-size: 1.4rem;
+	display: inline-block;
+
 	&:last-child {
 		margin-right: 0;
 	}
 `;
+
+const SliderWrapper = styled.div`
+	height: 6rem;
+	width: 100%;
+	margin: 0 auto;
+	overflow: hidden;
+	position: relative;
+`;
+
+const SliderViewScope = styled.div`
+	width: 95%;
+	height: 6rem;
+	overflow: hidden;
+	margin: 0 auto;
+`;
+
+const LeftArrow = styled(FontIcon)`
+	position: absolute;
+	color: #ff790a;
+	top: 1.3rem;
+	left: 0;
+`;
+
+const RightArrow = styled(FontIcon)`
+	color: #ff790a;
+	position: absolute;
+	top: 1.3rem;
+	right: 0;
+`;
+
 const TagSection = (props) => {
+	const sliderSettings = {
+		slidesToShow: 7,
+		infinite: false,
+		prevArrow: <LeftArrow name="arrow-alt-left" />,
+		nextArrow: <RightArrow name="arrow-alt-right" />,
+	};
+
 	return (
-		props.tags.map((tag) => {
-			return (
-				<TagItemWrapper key={tag.title} >
-					<Tag {...tag} />
-				</TagItemWrapper>);
-		})
+		<SliderWrapper>
+			<SliderViewScope>
+				<Slider {...sliderSettings}>
+					{
+						props.tags.map((tag) => {
+							return (
+								<TagItemWrapper key={tag.title} >
+									<Tag {...tag} />
+								</TagItemWrapper>
+							);
+						})
+					}
+				</Slider>
+			</SliderViewScope>
+		</SliderWrapper>
 	);
 };
 
