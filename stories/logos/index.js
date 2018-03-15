b65b711abdda1b16f7969493f2e6e0033598d50c
hakhag <hakonhagen2@gmail.com>
2018-03-15 15:25:31 +0100

    More fixes for KKLogo
    Possible to use round variant with `hasCircle=true`

--- stories/logos/index.js
+++ stories/logos/index.js
@@ -2,13 +2,21 @@ import { storiesOf } from '@storybook/react';
 import { StorybookPaddedGrid } from '../../src/storybook-decorators/PaddedGrid';
 
 import { ShinyStory } from './ShinyStory';
+<<<<<<< HEAD
 import { SeHerStory } from './SeHerStory';
 import { DagbladetStory } from './DagbladetStory';
+=======
+import { KKStory } from './KKStory';
+>>>>>>> More fixes for KKLogo
 
 export default () => {
 	storiesOf('Logos', module)
 		.addDecorator(StorybookPaddedGrid)
 		.add('ShinyLogo', ShinyStory)
+<<<<<<< HEAD
 		.add('SeHerLogo', SeHerStory)
+=======
+		.add('KKLogo', KKStory)
+>>>>>>> More fixes for KKLogo
 		.add('DagbladetLogo', DagbladetStory);
 };
