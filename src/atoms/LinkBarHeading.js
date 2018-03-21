a0b41d978b6fe9790fb5422bd170d0a13234b539
misund <thomas@hemmeligadresse.com>
2018-03-21 11:51:12 +0100

    feature: Create sizes in LinkBarHeading that matches Link and Button

--- src/atoms/LinkBarHeading.js
+++ src/atoms/LinkBarHeading.js
@@ -1,5 +1,9 @@
+import React from 'react';
 import { LinkBarLinkBase } from './LinkBarLinkBase';
 
 const LinkBarHeading = LinkBarLinkBase.withComponent('span');
 
+export const XSmallLinkBarHeading = props => <LinkBarHeading size="xsmall" {...props} />;
+export const SmallLinkBarHeading = props => <LinkBarHeading size="small" {...props} />;
 export { LinkBarHeading };
+export const LargeLinkBarHeading = props => <LinkBarHeading size="large" {...props} />;
