9fb997380021efe12fbcea36b3a7ec24c3109f3f
Tanya Litvinenko <tanyalitvinenko976@gmail.com>
2018-04-24 11:45:28 +0300

    set background parameter default value using theme

--- src/atoms/LinkBarLinkBordered.js
+++ src/atoms/LinkBarLinkBordered.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { LinkBarLink } from '..';
 import { stringToRGB } from '../utils';
 
-const calculateTextColor = (backgroundColor, theme) => {
+const calculateTextColor = (theme, backgroundColor=theme.colors.primary) => {
 	const { r, g, b } = stringToRGB(backgroundColor);
 	const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000; // converting to YIQ to determine brightness of background
 	return (yiq >= 150) ? theme.colors.typeGray : theme.colors.white;
@@ -23,20 +23,20 @@ const LinkBarLinkBordered = styled(LinkBarLink)`
 		props.isActive && `
 			border-color: ${props.activeBackground || props.theme.colors.primary};
 			background-color: ${props.activeBackground || props.theme.colors.primary};
-			color: ${calculateTextColor(props.activeBackground || props.theme.colors.primary, props.theme)} !important;
+			color: ${calculateTextColor(props.theme, props.activeBackground)} !important;
 		`
 	)}
 	
 	&:hover{
 		border-color: ${props => props.activeBackground || props.theme.colors.primary};
 		background-color: ${props => props.activeBackground || props.theme.colors.primary};
-		color: ${props => calculateTextColor(props.activeBackground || props.theme.colors.primary, props.theme)} !important;
+		color: ${props => calculateTextColor(props.theme, props.activeBackground)} !important;
 	};	
 	
 	&:focus{
 		border-color: ${props => props.activeBackground || props.theme.colors.primary} !important;
 		background-color: ${props => props.activeBackground || props.theme.colors.primary} !important;
-		color: ${props => calculateTextColor(props.activeBackground || props.theme.colors.primary, props.theme)} !important;
+		color: ${props => calculateTextColor(props.theme, props.activeBackground)} !important;
 	};
 `;
 
