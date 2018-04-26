ed249426e41a3d768a62397fafde10c82d9694a8
LupakIvan <lupakivan92@gmail.com>
2018-04-26 13:17:26 +0300

    Removed useless code (#408)
    * Removed useless code
    
    * Code refactoring
    
    * Refactoring
    
    * docs(readme): Fix typos
    
    * ci(percy): Run percy on master/node 9; see #414, see #415

--- stories/MatComponents/index.js
+++ stories/MatComponents/index.js
@@ -2,7 +2,6 @@ import { storiesOf } from '@storybook/react';
 
 import MatColophon from './MatColophon';
 import description from './mat-description';
-import MainRecipe from './MainRecipe';
 import MoreComponent from './mat-more-component';
 import SingleIngredient from './SingleIngredient';
 import Footer from './footer';
@@ -12,7 +11,6 @@ export default () => {
 		.add('Colophon', MatColophon)
 		.add('Mat Description', description)
 		.add('More Component', MoreComponent)
-		.add('MainRecipe', MainRecipe)
 		.add('SingleIngredient', SingleIngredient)
 		.add('Footer', Footer);
 };
