eb9e32b1181bcacf08569bdd1d284bf7fefbbe31
sergeyvlakh <sevl@ciklum.com>
2018-05-02 17:59:30 +0300

    Unexpected whitespace :0

--- src/atoms/SvgIcon/SvgIconWrapper.js
+++ src/atoms/SvgIcon/SvgIconWrapper.js
@@ -3,7 +3,7 @@ import Styled from 'styled-components';
 const SvgIconWrapper = Styled.div`
 	display: inline-block;
 	text-align: center;
-	width: ${props => props.size }rem;
+	width: ${props => props.size}rem;
 	
 	${props => (props['size-sm'] && `
 	@media screen and (max-width: ${props.theme.flexboxgrid.breakpoints.md}em) {
