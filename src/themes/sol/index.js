de7e40f321d871db0b3a824148e042bd0559480c
jimoe <jim@sol.no>
2018-05-16 12:48:17 +0200

    Update index.js

--- src/themes/sol/index.js
+++ src/themes/sol/index.js
@@ -3,7 +3,7 @@ import { css } from 'styled-components';
 import colors from './colors';
 import variables from './variables';
 
-// TODO: A pemanent solution for the a underline should be made. 
+// TODO: A pemanent solution for the a underline should be made.
 //       se: https://github.com/dbmedialab/wolverine-frontend/issues/460
 const global = css`
 	@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700,800');
