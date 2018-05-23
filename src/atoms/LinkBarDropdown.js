48b916b43e2e3870c67426ca933dc5c539a1b824
abaz <abaz@ciklum.com>
2018-05-23 10:01:04 +0300

    features #308; percy fix

--- src/atoms/LinkBarDropdown.js
+++ src/atoms/LinkBarDropdown.js
@@ -43,7 +43,7 @@ class Dropdown extends React.Component {
 				onClick={this.toggle}
 				{...rest}
 			>
-				{linkText}{` `}
+				{linkText}{' '}
 				<FontIcon name={`arrow-alt-${updown}`} />
 			</Button>
 		);
