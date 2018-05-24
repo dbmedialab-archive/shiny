1c55bc9216ca656f5297a940d3bdf4e2f0876ac3
Just Thomas Hiorth Misund <thomas@hemmeligadresse.com>
2018-05-24 15:48:39 +0200

    fix: Specify ui font sizes for Seher theme

--- src/themes/seher/variables.js
+++ src/themes/seher/variables.js
@@ -1,4 +1,11 @@
 export const variables = {
 	mainFont: '"Source Sans Pro", sans-serif',
 	headingsFont: '"Source Sans Pro", sans-serif',
+	
+	uiRegularSize: '1.4rem',
+	uiRegularLineHeight: '1.9rem',
+	uiSmallSize: '1.2rem',
+	uiSmallLineHeight: '1.7rem',
+	uiTinySize: '1.0rem',
+	uiTinyLineHeight: '1.4rem',
 };
