9330c516162acbfb2e27794eefda436a4bcd5281
misund <thomas@hemmeligadresse.com>
2018-06-05 12:17:04 +0200

    fix(LinkBarButton): Remove hard padding override

--- src/atoms/LinkBarButtonBase.js
+++ src/atoms/LinkBarButtonBase.js
@@ -9,7 +9,5 @@ const Button = BlockLink.withComponent('button');
 const SomewhatPrettyButton = LinkBarLinkBase.withComponent(Button);
 export const LinkBarButtonBase = styled(SomewhatPrettyButton)`
 	position: relative;
-	padding-top: calc(1/2*${props => props.theme.variables.verticalBase});
-	padding-bottom: calc(1/2*${props => props.theme.variables.verticalBase});
 	cursor: pointer;
 	`;
