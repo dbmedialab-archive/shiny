e6e92668f59ef3aba9e59ddce0b4583084cc24c9
misund <thomas@hemmeligadresse.com>
2018-06-14 19:08:11 +0200

    refactor: Remove workaround after LoicMahieu/react-styled-flexboxgrid#87

--- src/atoms/Grid.js
+++ src/atoms/Grid.js
@@ -1,12 +1,1 @@
-import styled from 'styled-components';
-import { Grid as LibGrid } from 'react-styled-flexboxgrid';
-
-// react-styled-flexboxgrid doesn't apply outerMargin to non-fluid grids.
-// We need to add this to fit wallpapers and topbanners.
-
-const Grid = styled(LibGrid)`
-	padding-left: ${props => props.theme.flexboxgrid.outerMargin}rem;
-	padding-right: ${props => props.theme.flexboxgrid.outerMargin}rem;
-`;
-
-export { Grid };
+export { Grid } from 'react-styled-flexboxgrid';
