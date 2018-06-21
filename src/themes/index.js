5f018b8cec89074a3f37388b93b4a7b575bc0db9
misund <thomas@hemmeligadresse.com>
2018-06-21 12:06:58 +0200

    feature: Add light theme

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,4 +1,6 @@
 import defaultTheme from './default-theme';
+import lightTheme from './light-theme';
+
 import dagbladet from './dagbladet';
 import dinside from './dinside';
 import kk from './kk';
@@ -7,10 +9,13 @@ import sol from './sol';
 import topp from './topp';
 import mat from './mat';
 
+
 export { themePropTypes } from './theme-prop-types';
 
 const themes = {
 	defaultTheme,
+	lightTheme,
+
 	dagbladet,
 	dinside,
 	kk,
---
+++ src/themes/light-theme/index.js
@@ -0,0 +1,84 @@
+import { darken, lighten } from 'polished';
+
+const variables = {
+	mainFont: '"Roboto","Helvetica",Helvetica,Arial,sans-serif',
+	headingsFont: '"Roboto","Helvetica",Helvetica,Arial,sans-serif',
+	headingsWeight: '300',
+};
+
+const colorsToShade = {
+	primary: '#333',
+	secondary: '#f8f8f8',
+
+	blue: '#185bd3',
+	type: '#666',
+
+
+	pink: '#444',
+	purple: '#444',
+	teal: '#444',
+	green: '#444',
+	yellow: '#444',
+	orange: '#444',
+	navy: '#444',
+	burgundy: '#444',
+	brown: '#444',
+	sand: '#444',
+};
+
+// Creates 4 shades of each color in colorsToShade
+// For example: primary becomes primary, primaryDark, primaryLight and primaryLighter
+const shadedColors = Object.keys(colorsToShade).map(color => ({
+	[`${color}`]: colorsToShade[color],
+	[`${color}Dark`]: darken(0.2, colorsToShade[color]),
+	[`${color}Light`]: lighten(0.15, colorsToShade[color]),
+	[`${color}Lighter`]: lighten(0.3, colorsToShade[color]),
+}));
+
+const combinedShadedColors = shadedColors.reduce((acc, cur) => Object.assign(acc, cur), {});
+
+const skinColors = {
+	splashBackground: 'primary',
+	splashBorder: 'primary',
+	splashText: 'white',
+
+	calmBackground: 'secondary',
+	calmBorder: 'secondary',
+	calmText: 'type',
+
+	heading: 'primary',
+};
+
+const colors = {
+	...combinedShadedColors,
+	skinColors,
+	link: '#185bd3',
+};
+
+const global = `
+	@import url('https://fonts.googleapis.com/css?family=Roboto:300,700,800');
+
+	* {
+		box-sizing: border-box;
+	}
+
+	body {
+		color: ${colors.type}
+		padding: 0;
+		margin: 0;
+		font-family: ${variables.mainFont};
+		font-weight: 300;
+		font-style: normal;
+		line-height: 1.5;
+		position: relative;
+		cursor: auto;
+	}
+`;
+
+export default {
+	name: 'Light Theme',
+	global,
+	colors,
+	variables,
+	flexboxgrid: {},
+};
