1b4be638258ded1db2f894901172f4b459ead72b
jimoe <jim@sol.no>
2018-07-10 16:00:52 +0200

    feature: Add theme for start.no (#589)
    * Adding theme for start.no
    
    * switched primary and secondary on start.no
    
    * Adding theme 'start' to the list
    
    * Background-color
    
    * fix(start): Change name from sol to start
    
    * fix: Simplify start theme
    
    * fix: start theme fonts
    
    * fix: Add skinColors to start theme

---
+++ src/themes/start/colors.js
@@ -0,0 +1,27 @@
+import { darken, lighten } from 'polished';
+
+const colorsToShade = {
+	// Brand colors
+	primary: '#313131',
+	secondary: '#7a9a26',
+
+	grayTint: '#eee',
+};
+
+// Creates 4 shades of each color in colorsToShade
+// For example: primary becomes primary, primaryDark, primaryLight and primaryLighter
+const shadedColors = Object.keys(colorsToShade).map(color => ({
+	[`${color}`]: colorsToShade[color],
+	[`${color}Dark`]: darken(0.2, colorsToShade[color]),
+	[`${color}Light`]: lighten(0.15, colorsToShade[color]),
+	[`${color}Lighter`]: lighten(0.3, colorsToShade[color]),
+}));
+
+const skinColors = {
+	type: 'darkness',
+};
+
+export default {
+	...shadedColors,
+	skinColors,
+};
---
+++ src/themes/start/index.js
@@ -0,0 +1,25 @@
+import { cssReset } from '../../utils/css-reset';
+
+import colors from './colors';
+import variables from './variables';
+
+const global = `
+	@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400');
+
+	${cssReset}
+
+	body {
+		color: ${colors[colors.skinColors.type]};
+		font-family: ${variables.mainFont};
+		font-size: ${variables.uiRegularSize};
+		line-height: ${variables.uiRegularLineHeight};
+		background-color: ${colors.grayTintLighter};
+	}
+`;
+
+export default {
+	name: 'Start',
+	global,
+	colors,
+	variables,
+};
