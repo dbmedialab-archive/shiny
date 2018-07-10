fea11e59fac647e977a7b959793bdbcece82b903
misund <thomas@hemmeligadresse.com>
2018-07-10 15:35:53 +0200

    fix: start theme fonts

--- src/themes/start/variables.js
+++ src/themes/start/variables.js
@@ -1,6 +1,6 @@
 export default {
-	mainFont: '"Roboto","Helvetica",Helvetica,Arial,sans-serif',
-	headingsFont: '"Roboto","Helvetica",Helvetica,Arial,sans-serif',
+	mainFont: "'Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+	headingsFont: '"Montserrat", "Helvetica Neue", Helvetica, Arial, sans-serif',
 
 	uiRegularSize: '1.6rem',
 	uiRegularLineHeight: '2.4rem',
