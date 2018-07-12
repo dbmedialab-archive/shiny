cced169e3925732bb92299dd049f5bfc960998e2
sergeyvlakh <sevl@ciklum.com>
2018-07-12 13:08:00 +0300

    FIX/Removed id's from svg icon.

--- src/atoms/SvgIcon/Difficulty.js
+++ src/atoms/SvgIcon/Difficulty.js
@@ -3,33 +3,28 @@ import PropsTypes from 'prop-types';
 
 const Difficulty = props => (
 	<svg width="100%" height="100%" viewBox="-10 -20 50 50">
-		<g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-			<g id="nivå-stor/enkel">
+		<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+			<g>
 				<g
-					id="nivå"
 					transform="translate(12.500000, 11.000000) scale(-1, 1) translate(-12.500000, -11.000000) "
 				>
 
 					<polygon
-						id="Fill-1"
 						fill="#333333"
 						points="0 22.0000005 6.60000014 22.0000005 6.60000014 0 0 0"
 					/>
 
 					<polygon
-						id="Fill-2-yes-this-difficult"
 						fill="#333333"
 						stroke="none"
 						points="8.80000019 22.0000005 15.4000003 22.0000005 15.4000003 5.50000012 8.80000019 5.50000012"
 					/>
 
 					<polygon
-						id="Fill-3"
 						fill="#333333"
 						points="17.6000004 22.0000005 24.2000005 22.0000005 24.2000005 11.0000002 17.6000004 11.0000002"
 					/>
 					<rect
-						id="Rectangle"
 						fill={props.value > 1 ? '#333333' : '#FAF9F9'}
 						x="9.90000021"
 						y="6.60000014"
@@ -37,7 +32,6 @@ const Difficulty = props => (
 						height="14.3000003"
 					/>
 					<rect
-						id="Rectangle-Copy"
 						fill={props.value > 2 ? '#333333' : '#FAF9F9'}
 						x="1.10000002"
 						y="1.10000002"
