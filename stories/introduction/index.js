0deae7e2116bca276d8372d5bbfce9bf4b81dac1
Sindre Øye Svendby <sinsvend@gmail.com>
2018-08-27 18:31:14 +0200

    ci fix: Remove unused code

--- stories/introduction/index.js
+++ stories/introduction/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { addDecorator } from '@storybook/react';
 import { StorybookPaddedGrid } from '../../src/storybook-decorators/PaddedGrid';
 
 import Welcome from './Welcome';
 import Themes from './Themes';
 import Palette from './Palette';
 import ThemeVariables from './ThemeVariables';
-
-// import { StorybookThemeSwitcher } from '../../src/storybook-decorators/ThemeSwitcher';
-
-// addDecorator(StorybookThemeSwitcher);
 
 export default () => {
 	storiesOf('Welcome', module)
