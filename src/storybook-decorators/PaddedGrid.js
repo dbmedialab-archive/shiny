841b1672fecefabee6871d502e580137fe7b3087
Sindre Øye Svendby <sinsvend@gmail.com>
2018-08-27 19:08:52 +0200

    Let's rewrite paddedGrid

--- src/storybook-decorators/PaddedGrid.js
+++ src/storybook-decorators/PaddedGrid.js
@@ -3,10 +3,18 @@ import styled from 'react-emotion';
 
 import { Grid } from '..';
 
-export const PaddedGrid = styled(Grid)`
+export const PaddedDiv = styled.div`
 	padding-top: calc(2*${props => props.theme.variables.verticalBase});
 	padding-bottom: calc(2*${props => props.theme.variables.verticalBase});
 `;
+
+const PaddedGrid = ({ children }) => (
+	<Grid>
+		<PaddedDiv>
+			{children}
+		</PaddedDiv>
+	</Grid>
+);
 
 const StorybookPaddedGrid = stories => <PaddedGrid>{stories()}</PaddedGrid>;
 
