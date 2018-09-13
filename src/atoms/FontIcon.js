52b800912ffbeb87cad6c38988afe0ab34365034
misund <thomas@hemmeligadresse.com>
2018-09-13 19:06:05 +0200

    fix(FontIcon): Fix typo 'pannel-open' to 'panel-open'

--- src/atoms/FontIcon.js
+++ src/atoms/FontIcon.js
@@ -495,7 +495,7 @@ const Icon = styled.span`
 		case 'people': return '\\e7ce';
 		case 'people-alt': return '\\e7cf';
 		case 'pause': return '\\e7d0';
-		case 'pannel-close': return '\\e7d1';
+		case 'panel-close': return '\\e7d1';
 		case 'panel-show': return '\\e7d2';
 		case 'page-restricted': return '\\e7d3';
 		case 'page-remove': return '\\e7d4';
