d1cacab86c303c6eafe822dafac597f95856e418
Simen Berge <egreb@users.noreply.github.com>
2018-09-14 15:24:43 +0200

    Introducing footer stories (#647)

---
+++ stories/footers/dinside.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { DinsideFooter } from '../../src/molecules/Footers/DinsideFooter';
+import { HugeHeading } from '../../src/atoms/Heading';
+
+export default () => (
+	<section>
+		<HugeHeading>Dinside Footer</HugeHeading>
+		<DinsideFooter />
+	</section>
+);
