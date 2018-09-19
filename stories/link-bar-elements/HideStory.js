7e0290ddeb270ac0f0503cf16e50c2265f5ec5c1
misund <thomas@hemmeligadresse.com>
2018-09-19 18:01:58 +0200

    docs: Use the info addon in link bar element stories

--- stories/link-bar-elements/HideStory.js
+++ stories/link-bar-elements/HideStory.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import { withInfo } from '@storybook/addon-info';
 import { DemoContainer } from '../storybook-components';
 
-import { Heading, HugeHeading } from '../../src/atoms/Heading';
-import { Code } from '../../src/atoms/Code';
-
-// HorizontalLinkBar
 import {
 	HorizontalLinkBar,
 	LinkBarLink,
+	SmallLinkBarLink,
+	LargeLinkBarLink,
 } from '../../src';
 
 const HideStory = () => (
 	<section>
-		<HugeHeading>Hidden LinkBar elements</HugeHeading>
-		<p>Elements in `VerticalLinkBar` and `HorizontalLinkBar` can be hidden with props that resemble the `Col` props.</p>
-		<p>Set `xs`, `sm`, `md` or `lg` to `false`, and the element will be hidden from that breakpoint and up.</p>
-		<p>Override with a higher breakpoint</p>
-
-		<Heading>Demo</Heading>
+		<DemoContainer>
+			<HorizontalLinkBar backgroundColor="white">
+				<SmallLinkBarLink linkText="All screens" url="https://example.com" />
+				<SmallLinkBarLink md={false} linkText="xs and sm screens" url="https://example.com" />
+				<SmallLinkBarLink xs={false} sm linkText="sm, md and lg screens" url="https://example.com" />
+				<SmallLinkBarLink xs={false} md linkText="md and lg screens" url="https://example.com" />
+				<SmallLinkBarLink xs={false} lg linkText="lg screens" url="https://example.com" />
+			</HorizontalLinkBar>
+		</DemoContainer>
 		<DemoContainer>
 			<HorizontalLinkBar backgroundColor="white">
 				<LinkBarLink linkText="All screens" url="https://example.com" />
@@ -27,25 +29,27 @@ const HideStory = () => (
 				<LinkBarLink xs={false} lg linkText="lg screens" url="https://example.com" />
 			</HorizontalLinkBar>
 		</DemoContainer>
-
-		<Heading>Usage</Heading>
-		<Code language="jsx">
-			{`
-import {
-	HorizontalLinkBar,
-	LinkBarLink,
-} from '@aller/shiny';
-
-<HorizontalLinkBar backgroundColor="white">
-	<LinkBarLink linkText="All screens" url="https://example.com" />
-	<LinkBarLink md={false} linkText="xs and sm screens" url="https://example.com" />
-	<LinkBarLink xs={false} sm linkText="sm, md and lg screens" url="https://example.com" />
-	<LinkBarLink xs={false} md linkText="md and lg screens" url="https://example.com" />
-	<LinkBarLink xs={false} lg linkText="lg screens" url="https://example.com" />
-</HorizontalLinkBar>
-			`}
-		</Code>
+		<DemoContainer>
+			<HorizontalLinkBar backgroundColor="white">
+				<LargeLinkBarLink linkText="All screens" url="https://example.com" />
+				<LargeLinkBarLink md={false} linkText="xs and sm screens" url="https://example.com" />
+				<LargeLinkBarLink xs={false} sm linkText="sm, md and lg screens" url="https://example.com" />
+				<LargeLinkBarLink xs={false} md linkText="md and lg screens" url="https://example.com" />
+				<LargeLinkBarLink xs={false} lg linkText="lg screens" url="https://example.com" />
+			</HorizontalLinkBar>
+		</DemoContainer>
 	</section>
 );
 
-export { HideStory };
+export default withInfo({
+	text: `
+<p>Hidden LinkBar elements</p>
+<p>Elements in VerticalLinkBar and HorizontalLinkBar can be hidden with
+	props that resemble the Col props.
+</p>
+<p>Set xs, sm, md or lg to false, and the element will be
+	hidden from that breakpoint and up.
+</p>
+<p>Override with a higher breakpoint.</p>
+	`,
+})(HideStory);
