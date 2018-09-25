2bc1180ba28c962f3b26b80859a2ed91c731abbd
misund <thomas@hemmeligadresse.com>
2018-09-25 16:30:14 +0200

    docs(HorizontalLinkBar): Document overflowing content

--- stories/link-bars/HorizontalLinkBarStory.js
+++ stories/link-bars/HorizontalLinkBarStory.js
@@ -38,6 +38,28 @@ import {
 </HorizontalLinkBar>
 					`}
 		</Code>
+
+		<Heading>Overflowing content</Heading>
+		<p>Here is another link bar, with enough content to overflow the allotted space.</p>
+		<DemoContainer>
+			<HorizontalLinkBar backgroundColor="white">
+				<LinkBarLink linkText="One" url="https://example.com" isActive />
+				<LinkBarLink linkText="Two" url="https://example.com" />
+				<LinkBarLink linkText="Buckle My Shoe" url="https://example.com" />
+				<LinkBarLink linkText="Three" url="https://example.com" isActive />
+				<LinkBarLink linkText="Four" url="https://example.com" />
+				<LinkBarLink linkText="Shut The Door" url="https://example.com" />
+				<LinkBarLink linkText="Five" url="https://example.com" isActive />
+				<LinkBarLink linkText="Six" url="https://example.com" />
+				<LinkBarLink linkText="Pick Up Sticks" url="https://example.com" />
+				<LinkBarLink linkText="Seven" url="https://example.com" isActive />
+				<LinkBarLink linkText="Eight" url="https://example.com" />
+				<LinkBarLink linkText="Lay Them Straight" url="https://example.com" />
+				<LinkBarLink linkText="Nine" url="https://example.com" isActive />
+				<LinkBarLink linkText="Ten" url="https://example.com" />
+				<LinkBarLink linkText="Begin Again" url="https://example.com" />
+			</HorizontalLinkBar>
+		</DemoContainer>
 	</section>
 );
 
