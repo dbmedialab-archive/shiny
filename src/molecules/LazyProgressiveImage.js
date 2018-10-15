2e77ec629c7f4f3d1411f474ecf167911b7adcda
Simen Berge <bergesimen@gmail.com>
2018-10-15 15:36:01 +0200

    reset LazyProgressiveImage component to master

--- src/molecules/LazyProgressiveImage.js
+++ src/molecules/LazyProgressiveImage.js
@@ -15,25 +15,6 @@ const Figure = styled.figure`
 	margin: 0;
 `;
 
-const FigCaption = styled.figcaption`
-	font-size: 1.4rem;
-	font-weight: 400;
-	border-bottom: 1px solid #f4f4f4;
-	display: block;
-	padding: 1.6rem .5rem;
-	position: absolute;
-	bottom: 0;
-	left: 0;
-	z-index: 1;
-	max-width: 100%;
-`;
-
-const figcaptionType = {
-	active: false,
-	intro: '',
-	text: '',
-};
-
 /**
  * LazyProgressiveImage downloads a small version of an image first, then loads the correct size when it is ready
  */
@@ -53,7 +34,6 @@ class LazyProgressiveImage extends PureComponent {
 		preventBlur: propTypes.bool,
 		/** Option to fadein the image. */
 		fadeIn: propTypes.bool,
-		figCaption: propTypes.instanceOf(figcaptionType),
 	}
 
 	static defaultProps = {
@@ -61,11 +41,6 @@ class LazyProgressiveImage extends PureComponent {
 		alt: 'Artikkelbilde.',
 		backgroundColor: '#ececec',
 		fadeIn: false,
-		figCaption: {
-			active: false,
-			intro: '',
-			text: '',
-		},
 	}
 
 	componentDidMount() {
@@ -80,15 +55,9 @@ class LazyProgressiveImage extends PureComponent {
 			ratio,
 			fadeIn,
 			children,
-			figCaption,
 			preventBlur,
 			backgroundColor,
 		} = this.props;
-
-		let figCaptionElement = null;
-		if (figCaption.intro && figCaption.text) {
-			figCaptionElement = <FigCaption><strong>{figCaption.intro}</strong> {figCaption.text}</FigCaption>;
-		}
 
 		return (
 			<Figure
@@ -107,7 +76,6 @@ class LazyProgressiveImage extends PureComponent {
 					/>
 					<noscript><img src={src} alt={alt} itemProp="image" /></noscript>
 				</Picture>
-				{figCaptionElement}
 			</Figure>
 		);
 	}
