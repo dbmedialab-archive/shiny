d5b7804f1ccb77acaf54a61ef5e9d01c57d2e3a2
misund <thomas@hemmeligadresse.com>
2018-10-18 16:43:13 +0200

    docs(HorizontalLinkBars): Use withInfo and split into two stories

--- stories/link-bars/HorizontalLinkBarStory.js
+++ stories/link-bars/HorizontalLinkBarStory.js
@@ -1,66 +1,25 @@
 import React from 'react';
-
-import { Heading, HugeHeading } from '../../src/atoms/Heading';
-import { Code } from '../../src/atoms/Code';
-import { DemoContainer } from '../storybook-components';
+import { withInfo } from '@storybook/addon-info';
 
 import {
 	LinkBarLink,
 	HorizontalLinkBar,
 } from '../../src';
 
+import { DemoContainer } from '../storybook-components';
+
 const HorizontalLinkBarStory = () => (
-	<section>
-		<HugeHeading>HorizontalLinkBar</HugeHeading>
-		<p>A horizontal navbar that can contain items such as links, buttons, dropdowns or other stuff.</p>
-
-		<Heading>Demo</Heading>
-		<DemoContainer>
-			<HorizontalLinkBar backgroundColor="white">
-				<LinkBarLink linkText="One" url="https://example.com" isActive />
-				<LinkBarLink linkText="Two" url="https://example.com" />
-				<LinkBarLink linkText="Buckle My Shoe" url="https://example.com" />
-			</HorizontalLinkBar>
-		</DemoContainer>
-
-		<Heading>Usage</Heading>
-		<Code language="jsx">
-			{`
-import {
-	HorizontalLinkBar,
-	LinkBarLink,
-} from '@aller/shiny';
-
-<HorizontalLinkBar backgroundColor="white">
-	<LinkBarLink linkText="One" url="https://example.com" isActive />
-	<LinkBarLink linkText="Two" url="https://example.com" />
-	<LinkBarLink linkText="Buckle My Shoe" url="https://example.com" />
-</HorizontalLinkBar>
-					`}
-		</Code>
-
-		<Heading>Overflowing content</Heading>
-		<p>Here is another link bar, with enough content to overflow the allotted space.</p>
-		<DemoContainer>
-			<HorizontalLinkBar backgroundColor="white">
-				<LinkBarLink linkText="One" url="https://example.com" isActive />
-				<LinkBarLink linkText="Two" url="https://example.com" />
-				<LinkBarLink linkText="Buckle My Shoe" url="https://example.com" />
-				<LinkBarLink linkText="Three" url="https://example.com" isActive />
-				<LinkBarLink linkText="Four" url="https://example.com" />
-				<LinkBarLink linkText="Shut The Door" url="https://example.com" />
-				<LinkBarLink linkText="Five" url="https://example.com" isActive />
-				<LinkBarLink linkText="Six" url="https://example.com" />
-				<LinkBarLink linkText="Pick Up Sticks" url="https://example.com" />
-				<LinkBarLink linkText="Seven" url="https://example.com" isActive />
-				<LinkBarLink linkText="Eight" url="https://example.com" />
-				<LinkBarLink linkText="Lay Them Straight" url="https://example.com" />
-				<LinkBarLink linkText="Nine" url="https://example.com" isActive />
-				<LinkBarLink linkText="Ten" url="https://example.com" />
-				<LinkBarLink linkText="Begin Again" url="https://example.com" />
-			</HorizontalLinkBar>
-		</DemoContainer>
-	</section>
+	<DemoContainer>
+		<HorizontalLinkBar backgroundColor="white">
+			<LinkBarLink linkText="One" url="https://example.com" isActive />
+			<LinkBarLink linkText="Two" url="https://example.com" />
+			<LinkBarLink linkText="Buckle My Shoe" url="https://example.com" />
+		</HorizontalLinkBar>
+	</DemoContainer>
 );
 
-export { HorizontalLinkBarStory };
+export default withInfo({
+	text: `
+<p>A horizontal navbar that can contain items such as links, buttons, dropdowns or other stuff.</p>
+	`,
+})(HorizontalLinkBarStory);
