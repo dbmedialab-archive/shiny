e3edf435e78347c6c8125b68d9ca704667d29b76
Sindre Øye Svendby <sinsvend@gmail.com>
2018-11-02 00:25:18 +0100

    Fix linting issues

--- src/themes/pregenerate.js
+++ src/themes/pregenerate.js
@@ -1,15 +1,25 @@
 import fs from 'fs';
 import path from 'path';
-import { themes } from  './index';
+import { themes } from './index';
 
-
-const themeNames = Object.keys(themes).filter(theme => !['defaultTheme', 'lightTheme', 'kk'].includes(theme));
+const themeNames = Object.keys(themes).filter(
+	theme => !['defaultTheme', 'lightTheme', 'kk'].includes(theme)
+);
 
 const prefix = 'module.exports = ';
 
 themeNames.forEach((name) => {
-	const indexFile = path.join(__dirname, '..', '..', 'lib', 'themes', name, 'index.js');
-	fs.writeFileSync(indexFile, prefix +  JSON.stringify(themes[name]));
+	const indexFile = path.join(
+		__dirname,
+		'..',
+		'..',
+		'lib',
+		'themes',
+		name,
+		'index.js'
+	);
+	fs.writeFileSync(indexFile, prefix + JSON.stringify(themes[name]));
+	// eslint-disable-next-line no-console
 	console.log(`creating a prebuild: ${indexFile}`);
 });
 
