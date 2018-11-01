3bddb129dd38a22a97fb450b34113355da9a2c59
Sindre Øye Svendby <sinsvend@gmail.com>
2018-11-01 21:01:32 +0100

    Add Pregenerate script

---
+++ src/themes/pregenerate.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { themes } from  './index';
+
+
+const themeNames = Object.keys(themes).filter(theme => !['defaultTheme', 'lightTheme', 'kk'].includes(theme));
+
+const prefix = 'module.export = ';
+
+themeNames.forEach((name) => {
+	// const indexFile = path.join(__dirname, '..', '..', 'lib', 'themes', name, 'index.js');
+	const indexFile = path.join(__dirname, name, 'index.js');
+	fs.writeFileSync(indexFile, prefix +  JSON.stringify(themes[name]));
+	console.log(`creating a prebuild: ${indexFile}`);
+});
+
+
+const kkIndexFile = path.join(__dirname, 'kk', 'index.js');
+fs.writeFileSync(kkIndexFile, `${prefix}{}`);
