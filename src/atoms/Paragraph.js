888747e50e68d9f038459043bdffc0e6006fce9a
misund <thomas@hemmeligadresse.com>
2018-11-02 12:44:47 +0100

    refactor(Paragraph): Simplify syntax

--- src/atoms/Paragraph.js
+++ src/atoms/Paragraph.js
@@ -1,29 +1,16 @@
-import React from 'react';
 import styled from 'react-emotion';
-import propTypes from 'prop-types';
 import { getVariable } from '../utils/get-variable';
 
-const P = styled.p`
+const Paragraph = styled.p`
 	font-size: ${getVariable('uiRegularSize')};
 	font-weight: ${getVariable('uiWeight')};
 	margin-bottom: ${getVariable('verticalBase')};
 	text-rendering: optimizeLegibility;
 	width: 100%;
+
 	strong {
 		font-weight: ${getVariable('uiWeightBold')};
 	}
 `;
-
-const Paragraph = (props) => {
-	const { children } = props;
-
-	return (
-		<P>{children}</P>
-	);
-};
-
-Paragraph.propTypes = {
-	children: propTypes.node.isRequired,
-};
 
 export { Paragraph };
