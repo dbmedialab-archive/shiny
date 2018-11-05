392c72d112a5a6342a482d721a6ba032d5d56661
Sindre Svendby <sinsvend@gmail.com>
2018-11-05 17:36:28 +0100

    build: Generate all themes except kk (#793)

--- src/themes/pregenerate.js
+++ src/themes/pregenerate.js
@@ -3,22 +3,32 @@ import path from 'path';
 import { themes } from './index';
 
 const themeNames = Object.keys(themes).filter(
-	theme => !['defaultTheme', 'lightTheme', 'kk'].includes(theme)
+	theme => !['defaultTheme', 'lightTheme'].includes(theme)
 );
+
+const themeFolderNames = [...themeNames, 'default-theme', 'light-theme'];
 
 const prefix = 'module.exports = ';
 const themeLibDir = path.join(__dirname, '..', '..', 'lib', 'themes');
 
-themeNames.forEach((name) => {
-	const indexFile = path.join(
-		themeLibDir,
-		name,
-		'index.js'
-	);
-	fs.writeFileSync(indexFile, prefix + JSON.stringify(themes[name]));
+themeFolderNames.forEach((folder) => {
+	const indexFile = path.join(themeLibDir, folder, 'index.js');
+	const themeName = getThemeName(folder);
+	fs.writeFileSync(indexFile, prefix + JSON.stringify(themes[themeName]));
 	// eslint-disable-next-line no-console
 	console.log(`creating a prebuild: ${indexFile}`);
 });
 
-const kkIndexFile = path.join(themeLibDir,  'kk', 'index.js');
+function getThemeName(folder) {
+	if (folder === 'default-theme') {
+		return 'defaultTheme';
+	}
+	if (folder === 'light-theme') {
+		return 'lightTheme';
+	}
+	return folder;
+}
+
+// kk index file is big, for performance reasons we create a fake empty index file
+const kkIndexFile = path.join(themeLibDir, 'kk', 'index.js');
 fs.writeFileSync(kkIndexFile, `${prefix}{}`);
