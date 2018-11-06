22f249a027d7b425659a07a5388f6b2ace778b46
Tehfvpr <33020245+sergeyvlakh@users.noreply.github.com>
2018-11-06 15:30:50 +0100

    test(Mat stories): Remove SingleIngredientStory (unreliable images, unused components) (#796)
    test(Mat stories): Remove SingleIngredientStory (unreliable images, unused components)

--- stories/MatComponents/index.js
+++ stories/MatComponents/index.js
@@ -3,12 +3,10 @@ import { StorybookPaddedGrid } from '../../src/storybook-decorators/PaddedGrid';
 
 import description from './mat-description';
 import MoreComponent from './mat-more-component';
-import SingleIngredient from './SingleIngredient';
 import Footer from './footer';
 
 export default () => {
 	storiesOf('Mat Components', module)
-		.add('SingleIngredient', SingleIngredient)
 		.add('Footer', Footer)
 		.addDecorator(StorybookPaddedGrid)
 		.add('Mat Description', description)
