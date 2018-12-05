f74e7624c532ed080bea73d7205e12e2aa92faba
misund <thomas@hemmeligadresse.com>
2018-12-05 17:37:03 +0100

    test(TrysilPlug): add stories for play button and custom labels

---
+++ stories/TrysilPlug/custom-labels.js
@@ -0,0 +1,45 @@
+/* eslint-disable max-len */
+import React from 'react';
+import styled from 'react-emotion';
+
+import { Heading, HugeHeading } from '../../src/atoms/Heading';
+import { TrysilPlug } from '../../src/molecules/TrysilPlug';
+
+import { getColor, getVariable } from '../../src/utils';
+
+const SpecialLabel = styled.div`
+	padding: ${getVariable('horizontalBase')};
+	background: ${getColor('primary')};
+	color: ${getColor('white')};
+	font-size: ${getVariable('uiRegularSize')};
+	line-height: ${getVariable('uiRegularLineHeight')};
+`;
+
+export default () => (
+	<section>
+		<HugeHeading>TrysilPlug</HugeHeading>
+		<p>The Labels component in TrysilPlug can be swapped out by sending in another component in the Labels prop.</p>
+
+		<Heading>Usage</Heading>
+		<TrysilPlug
+			Labels={SpecialLabel}
+			kicker="Another day, another video"
+			title="What should we plug today?"
+			subtitle="I guess we should plug another video again."
+			placeholderUrl="https://2.dbstatic.no/68712816.jpg?imageId=68712816&x=4.7222222222222&y=28.976572133169&cropw=90.740740740741&croph=60.419235511714&width=98&height=49&compression=30"
+			image="https://2.dbstatic.no/68712816.jpg?imageId=68712816&x=4.7222222222222&y=28.976572133169&cropw=90.740740740741&croph=60.419235511714&width=980&height=490&compression=70"
+			ratio={0.5}
+			url="https://example.com"
+			labels={[
+				{
+					text: 'Opinions',
+					backgroundColor: 'orange',
+				},
+				{
+					text: 'I am label',
+					backgroundColor: 'blue',
+				},
+			]}
+		/>
+	</section>
+);
