e1ece77b98e98221cf07b9acf8186e0cd0318db5
Alex Bazan <alexander-bazan@outlook.com>
2018-12-07 17:51:58 +0200

    CarouselSection: removed unnecessary styling

--- src/atoms/carousel/CarouselSection.js
+++ src/atoms/carousel/CarouselSection.js
@@ -4,7 +4,4 @@ export const CarouselSection = styled.section`
 	width: 100%;
 	position: relative;
 	height: ${props => (props.vertical ? props.verticalHeight : '')};
-	article {
-		margin: 0;
-	}
 `;
