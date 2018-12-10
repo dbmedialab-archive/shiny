b23dd59770a451430753df6e7a1197e8ca548e98
EvoX0 <rodionantonichev@gmail.com>
2018-12-10 16:58:01 +0200

    rerun percy tests

--- stories/TrysilPlug/with-play-button.js
+++ stories/TrysilPlug/with-play-button.js
@@ -16,7 +16,7 @@ export default () => (
 			title="What should we plug today?"
 			subtitle="I guess we should plug another video again."
 			placeholderUrl="https://2.dbstatic.no/68712816.jpg?imageId=68712816&x=4.7222222222222&y=28.976572133169&cropw=90.740740740741&croph=60.419235511714&width=98&height=49&compression=30"
-			image="https://2.dbstatic.no/68712816.jpg?imageId=68712816&x=4.7222222222222&y=28.976572133169&cropw=90.740740740741&croph=60.419235511714&width=980&height=490&compression=70"
+			image="https://picsum.photos/980/490"
 			ratio={0.5}
 			url="https://example.com"
 			labels={[
