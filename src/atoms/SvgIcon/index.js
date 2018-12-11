674eda8189368fb2047f9f25459a44b6278fac71
Just Thomas Hiorth Misund <thomas@hemmeligadresse.com>
2018-12-11 19:15:06 +0100

    feature(SvgIcon, TrysilPlug): Add play icon (#855)
    * test(TrysilPlug): add stories for play button and custom labels
    
    * feature(SvgIcon): Add play icon
    
    * feature(SvgIcon): Introduce AbsolutelyCenteredSvgIcon
    
    * feature(TrysilPlug): Add prop to display play icon over the plug image

--- src/atoms/SvgIcon/index.js
+++ src/atoms/SvgIcon/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
 import { SvgIconWrapper } from './SvgIconWrapper';
 
 import { ActivityTime        } from './ActivityTime';
@@ -32,7 +34,7 @@ import { CrossedVideoCamera  } from './CrossedVideoCamera';
 import { ExclamationMark     } from './ExclamationMark';
 import { YouTube             } from './YouTube';
 import { NotFound            } from './NotFound';
-
+import { Play                } from './Play';
 
 const SvgIcon = (props) => {
 	const {
@@ -114,6 +116,8 @@ const SvgIcon = (props) => {
 						return <ExclamationMark {...props} />;
 					case 'youtube':
 						return <YouTube {...props} />;
+					case 'play':
+						return <Play {...props} />;
 					default:
 						return null;
 					}
@@ -137,4 +141,17 @@ SvgIcon.defaultProps = {
 	'className': '',
 };
 
-export { SvgIcon };
+const AbsolutelyCenteredSvgIcon = styled(SvgIcon)`
+	position: absolute;
+	z-index: 1;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	margin: auto;
+`;
+
+export {
+	AbsolutelyCenteredSvgIcon,
+	SvgIcon,
+};
