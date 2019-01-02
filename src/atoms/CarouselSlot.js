e3b2dc13e9fb82e18e1fdbb74e1615e60ffb8876
misund <thomas@hemmeligadresse.com>
2019-01-02 17:24:10 +0100

    refactor(CarouselSlot): Refactor for readability

--- src/atoms/CarouselSlot.js
+++ src/atoms/CarouselSlot.js
@@ -6,25 +6,20 @@ import { getVariable } from '../utils/get-variable';
 
 export const CarouselSlot = styled(Col)`
 	${(props) => {
-		const base = css`
+		const horizontalBase = getVariable('horizontalBase')(props);
+		const verticalBase = getVariable('verticalBase')(props);
+
+		const marginRight = (!props.vertical) ? `calc(1/2 * ${horizontalBase})` : '0';
+		const marginBottom = (props.vertical) ? `calc(1/2 * ${verticalBase})` : '0';
+
+		return css`
+			margin-right: ${marginRight};
+			margin-bottom: ${marginBottom};
 			padding: 0;
 
 			&:last-child {
 				margin: 0;
 			}
 		`;
-
-		const directional = (!props.vertical)
-			? css`
-				margin-right: calc(1/2 * ${getVariable('horizontalBase')(props)});
-			` : css`
-				margin-bottom: calc(1/2 * ${getVariable('verticalBase')(props)});
-			`;
-
-		return css`
-			${base};
-			${directional};
-		`;
 	}}
-
 `;
