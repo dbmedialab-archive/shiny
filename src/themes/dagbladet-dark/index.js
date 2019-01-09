85f8981c785b9524950d1b475195e7cb3a426b28
misund <thomas@hemmeligadresse.com>
2019-01-09 17:29:58 +0100

    fix(dagbladet-dark): adjust colors after Mia feedback

--- src/themes/dagbladet-dark/index.js
+++ src/themes/dagbladet-dark/index.js
@@ -1,8 +1,15 @@
 import { merge } from 'aurora-deep-slice-merge';
+import { darken, lighten } from 'polished';
 import dagbladet from '../dagbladet';
 
+const darkness = '#1c1d25';
+
 const colors = merge(dagbladet.colors, {
-	type: '#efefef',
+	type: '#ffffff',
+	darkness,
+	darknessDark: darken(0.2, darkness),
+	darknessLight: lighten(0.15, darkness),
+	darknessLighter: lighten(0.3, darkness),
 
 	skinColors: Object.assign({}, dagbladet.colors.skinColors, {
 		background: 'darkness',
@@ -16,6 +23,7 @@ const colors = merge(dagbladet.colors, {
 
 		calmBackground: 'darknessLight',
 		calmText: 'type',
+		link: 'type',
 	}),
 });
 
