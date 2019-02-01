de67d6456f29c0b5bc44cd7e69c080de35707bea
Jon Inge Moe <jim@sol.no>
2019-02-01 14:16:52 +0100

    Adjustments

--- src/themes/elbil24/index.js
+++ src/themes/elbil24/index.js
@@ -8,7 +8,7 @@ const variables = {
 };
 
 const global = `
-	@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800');
+	@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
 
 	${cssReset}
 `;
