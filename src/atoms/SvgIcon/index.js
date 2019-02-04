21677a572a76bd11f516422c2d302c012148f440
EvoX0 <rodionantonichev@gmail.com>
2019-02-04 16:35:36 +0200

    SvgIcon code splitting

--- src/atoms/SvgIcon/index.js
+++ src/atoms/SvgIcon/index.js
@@ -1,136 +1,56 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 
 import { SvgIconWrapper } from './SvgIconWrapper';
 
-const ActivityTime = React.lazy(() => import('./ActivityTime'));
-const Difficulty = React.lazy(() => import('./Difficulty'));
-const Nut = React.lazy(() => import('./Nut'));
-const Fish = React.lazy(() => import('./Fish'));
-const Gluten = React.lazy(() => import('./Gluten'));
-const Pork = React.lazy(() => import('./Pork'));
-const TotalTime = React.lazy(() => import('./TotalTime'));
-const Vegan = React.lazy(() => import('./Vegan'));
-const Vegetarian = React.lazy(() => import('./Vegetarian'));
-const DoubleArrow = React.lazy(() => import('./DoubleArrow'));
-const Star = React.lazy(() => import('./Star'));
-const Lactose = React.lazy(() => import('./Lactose'));
-const Egg = React.lazy(() => import('./Egg'));
-const ShellFish = React.lazy(() => import('./ShellFish'));
-const Mail = React.lazy(() => import('./Mail'));
-const Pinterest = React.lazy(() => import('./Pinterest'));
-const CircleWithText = React.lazy(() => import('./CircleWithText'));
-const FaceBook = React.lazy(() => import('./FaceBook'));
-const Twitter = React.lazy(() => import('./Twitter'));
-const User = React.lazy(() => import('./Avatar'));
-const KK = React.lazy(() => import('./KK'));
-const Aller = React.lazy(() => import('./Aller'));
-const Dagbladet = React.lazy(() => import('./Dagbladet'));
-const OppskriftLogo = React.lazy(() => import('./OppskriftLogo'));
-const OppskriftMobileLogo = React.lazy(() => import('./OppskriftMobileLogo'));
-const OppskriftLoader = React.lazy(() => import('./OppskriftLoader'));
-const CrossedVideoCamera = React.lazy(() => import('./CrossedVideoCamera'));
-const ExclamationMark = React.lazy(() => import('./ExclamationMark'));
-const YouTube = React.lazy(() => import('./YouTube'));
-const NotFound = React.lazy(() => import('./NotFound'));
-const Play = React.lazy(() => import('./Play'));
-const DagbladetVideoIcon = React.lazy(() => import('./DagbladetVideo'));
-const BackNavIcon = React.lazy(() => import('./BackNav'));
+/* eslint-disable global-require */
+const icons = {
+	'activity': () => require('./ActivityTime').default,
+	'difficulty': () => require('./Difficulty').default,
+	'nut': () => require('./Nut').default,
+	'fish': () => require('./Fish').default,
+	'gluten': () => require('./Gluten').default,
+	'pork': () => require('./Pork').default,
+	'total-time': () => require('./TotalTime').default,
+	'vegan': () => require('./Vegan').default,
+	'vegetarian': () => require('./Vegetarian').default,
+	'star': () => require('./Star').default,
+	'laktose': () => require('./Lactose').default,
+	'egg': () => require('./Egg').default,
+	'skalldyr': () => require('./ShellFish').default,
+	'mail': () => require('./Mail').default,
+	'twitter': () => require('./Twitter').default,
+	'double-arrow': () => require('./DoubleArrow').default,
+	'pinterest': () => require('./Pinterest').default,
+	'circle-with-text': () => require('./CircleWithText').default,
+	'facebook': () => require('./FaceBook').default,
+	'user': () => require('./Avatar').default,
+	'kk': () => require('./KK').default,
+	'aller': () => require('./Aller').default,
+	'dagbladet': () => require('./Dagbladet').default,
+	'oppskrift-logo': () => require('./OppskriftLogo').default,
+	'oppskrift-mobile-logo': () => require('./OppskriftMobileLogo').default,
+	'oppskrift-loader': () => require('./OppskriftLoader').default,
+	'crossed-video-camera': () => require('./CrossedVideoCamera').default,
+	'exclamation': () => require('./ExclamationMark').default,
+	'youtube': () => require('./YouTube').default,
+	'play': () => require('./Play').default,
+	'dagbladetVideo': () => require('./DagbladetVideo').default,
+	'backNav': () => require('./BackNav').default,
+	'not-found': () => require('./NotFound').default,
+	'elseDiff': () => () => <div />,
+};
+/* eslint-enable global-require */
 
-const SvgIcon = (props) => {
-	const {
-		className,
-		color,
-		name,
-		size,
-		...rest
-	} = props;
+const SvgIcon = ({ name, ...props }) => {
+	const resolveIcon = icons[name] || icons.elseDiff;
+	const Icon = resolveIcon();
 
-	// @TODO camelCase the prop "size-sm" so that we can use it just like other props
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<SvgIconWrapper size={size} size-sm={rest['size-sm']} color={color} className={className}>
-				{
-					(() => {
-						switch (name) {
-						case 'pinterest':
-							return <Pinterest {...props} />;
-						case 'mail':
-							return <Mail {...props} />;
-						case 'skalldyr':
-							return <ShellFish {...props} />;
-						case 'egg':
-							return <Egg {...props} />;
-						case 'laktose':
-							return <Lactose {...props} />;
-						case 'activity':
-							return <ActivityTime {...props} />;
-						case 'difficulty':
-							return <Difficulty {...props} />;
-						case 'nut':
-							return <Nut {...props} />;
-						case 'fish':
-						case 'fisk':
-							return <Fish {...props} />;
-						case 'gluten':
-							return <Gluten {...props} />;
-						case 'pork':
-						case 'uten-svinekjott':
-							return <Pork {...props} />;
-						case 'total-time':
-							return <TotalTime {...props} />;
-						case 'vegan':
-							return <Vegan {...props} />;
-						case 'vegetar':
-						case 'vegetarian':
-							return <Vegetarian {...props} />;
-						case 'double-arrow':
-							return <DoubleArrow {...props} />;
-						case 'star':
-							return <Star {...props} />;
-						case 'user':
-							return <User {...props} />;
-						case 'facebook':
-							return <FaceBook {...props} />;
-						case 'twitter':
-							return <Twitter {...props} />;
-						case 'kk':
-							return <KK {...props} />;
-						case 'aller':
-							return <Aller {...props} />;
-						case 'dagbladet':
-							return <Dagbladet {...props} />;
-						case 'oppskrift-logo':
-							return <OppskriftLogo {...props} />;
-						case 'oppskrift-mobile-logo':
-							return <OppskriftMobileLogo {...props} />;
-						case 'oppskrift-loader':
-							return <OppskriftLoader {...props} />;
-						case 'circle-with-text':
-							return <CircleWithText {...props} />;
-						case 'crossed-video-camera':
-							return <CrossedVideoCamera {...props} />;
-						case 'not-found':
-							return <NotFound {...props} />;
-						case 'exclamation-mark':
-						case 'exclamation-point':
-						case 'exclamation':
-							return <ExclamationMark {...props} />;
-						case 'youtube':
-							return <YouTube {...props} />;
-						case 'play':
-							return <Play {...props} />;
-						case 'dagbladetVideo':
-							return <DagbladetVideoIcon {...props} />;
-						case 'backNav':
-							return <BackNavIcon {...props} />;
-						default:
-							return null;
-						}
-					})()}
-			</SvgIconWrapper>
-		</Suspense>
+		<SvgIconWrapper {...props}>
+			<Icon {...props} />
+		</SvgIconWrapper>
 	);
 };
 
