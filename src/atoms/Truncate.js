f3ee2aa2fb971edba73e4612081ee889b922134d
Roman Dontsov <rodo@ciklum.com>
2019-04-03 11:56:42 +0300

    Refactor(Truncate): Change max-width on very small screen resolutions

--- src/atoms/Truncate.js
+++ src/atoms/Truncate.js
@@ -1,16 +1,14 @@
 import styled from '@emotion/styled';
 import { getVariable } from '../utils/get-variable';
 
+
 export const Truncate = styled.span`
 	height: 100%;
 	white-space: nowrap;
 	overflow: hidden;
 	text-overflow: ellipsis;
 	max-width: calc(${getVariable('horizontalBase')} * 15);
-	@media screen and (max-width: ${({ theme }) => theme.flexboxgrid.breakpoints.sm}rem) {
-		max-width: calc(${getVariable('horizontalBase')} * 10);
-	}
-	@media screen and (max-width: ${({ theme }) => theme.flexboxgrid.breakpoints.xs}rem) {
+	@media screen and (max-width: ${({ theme }) => (theme.flexboxgrid.breakpoints.sm / 2)}rem) {
 		max-width: calc(${getVariable('horizontalBase')} * 5);
 	}
 `;
