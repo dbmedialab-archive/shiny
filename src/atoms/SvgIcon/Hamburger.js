933686a2a2aa9f8ccd51831532b2eb3afe6f844c
Roman Dontsov <rodo@ciklum.com>
2019-04-16 19:23:33 +0300

    feature(svgIcon Hamburger): Add property `opened` to Hamburger, set default color. Add story for new icon to LinkBarDropDown stories

--- src/atoms/SvgIcon/Hamburger.js
+++ src/atoms/SvgIcon/Hamburger.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 import { getColor } from '../../utils';
 
 const Hamburger = props => (
@@ -9,25 +11,29 @@ const Hamburger = props => (
 		<rect x="0" y="18" width="23" height="5" />
 	</svg>
 );
+Hamburger.propTypes = {
+	opened: PropTypes.bool,
+	color: PropTypes.string,
+};
+Hamburger.defaultProps = {
+	opened: false,
+	color: 'primary',
+};
 
 export default styled(Hamburger)`
-	&& {
-		rect {
-			fill: ${props => getColor(props.color)(props)};
-		}
-		*[aria-expanded='true'] {
-			& rect {
-				transition: .1s transform;
-				& :nth-child(1) {
-					transform: rotate(45deg) scale(1.17, 1) translateY(-2.5px) translateX(2.5px);
-				}
-				& :nth-child(2) {
-					transform: scale(0.2, 1) translateX(47px);
-				}
-				& :nth-child(3) {
-					transform: rotate(-45deg) scale(1.17, 1) translateY(-4px) translateX(-11.5px);
-				}
+	rect {
+		fill: ${props => getColor(props.color)(props)};
+		transition: .1s transform;
+		${({ opened }) => opened && css`
+			& :nth-child(1) {
+				transform: rotate(45deg) scale(1.17, 1) translateY(-2.5px) translateX(2.5px);
 			}
-		}
+			& :nth-child(2) {
+				transform: scale(0.2, 1) translateX(47px);
+			}
+			& :nth-child(3) {
+				transform: rotate(-45deg) scale(1.17, 1) translateY(-4px) translateX(-11.5px);
+			}
+		`}
 	}
 `;
