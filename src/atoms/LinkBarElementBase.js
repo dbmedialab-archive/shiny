aba6f0fd1d62164b64fb009382f374e91dd06450
Just Thomas Hiorth Misund <thomas@hemmeligadresse.com>
2019-04-24 12:39:50 +0200

    refactor(LinkBarElementBase): Divvy up into logical pieces (#1134)
    The LinkBarElementBase was a unyielding beast. This
    refactor splits it up into smaller pieces that can
    more easily be turned into shared utils later on.

--- src/atoms/LinkBarElementBase.js
+++ src/atoms/LinkBarElementBase.js
@@ -8,13 +8,30 @@ import { getColor, getVariable } from '../utils';
 
 const log = debug('LinkBarElementBase');
 
-const getTextColor = ({
-	isActive, activeTextColor, textColor,
-}) => (
-	(isActive && activeTextColor)
-		? getColor(activeTextColor)
-		: getColor(textColor)
-);
+const textColor = (props) => {
+	const {
+		isActive, activeTextColor, textColor,
+	} = props;
+
+	const color = (
+		(isActive && activeTextColor)
+			? getColor(activeTextColor)(props)
+			: getColor(textColor)(props)
+	);
+
+	const hoverColor = getColor(activeTextColor || textColor)(props);
+
+	return css`
+		&, &:visited {
+			color: ${color};
+
+			&:hover,
+			&:focus {
+				color: ${hoverColor};
+			}
+		}
+	`;
+};
 
 const getFocusBackground = (props) => {
 	const activeBackgroundFromProps = (
@@ -24,70 +41,187 @@ const getFocusBackground = (props) => {
 	return (activeBackgroundFromProps === 'transparent' ? 'rgba(0,0,0,.04)' : activeBackgroundFromProps);
 };
 
-export const LinkBarElementBase = styled.a`
-	display: inline-block;
+/*
+ * With the 'xsmall', 'small', 'medium' (default) and 'large' variants,
+ * LinkBar items like LinkBarLinks, LinkBarButtons and LinkBarDropdowns
+ * all take up the same vertical space. That way, you can stuff in
+ * another item without changing the height of your LinkBar.
+ */
+const linkBarElementSizes =	(props) => {
+	const { size, inset } = props;
+	const horizontalBase = getVariable('horizontalBase')(props);
+	const verticalBase = getVariable('verticalBase')(props);
+	const uiSmallSize = getVariable('uiSmallSize')(props);
+	const uiSmallLineHeight = getVariable('uiSmallLineHeight')(props);
+	const uiRegularSize = getVariable('uiRegularSize')(props);
+	const uiRegularLineHeight = getVariable('uiRegularLineHeight')(props);
 
-	/*
-	 * With the 'xsmall', 'small', 'medium' (default) and 'large' variants,
-	 * LinkBar items like LinkBarLinks, LinkBarButtons and LinkBarDropdowns
-	 * all take up the same vertical space. That way, you can stuff in
-	 * another item without changing the height of your LinkBar.
-	 */
-	${(props) => {
-		const { size, inset } = props;
-		const horizontalBase = getVariable('horizontalBase')(props);
-		const verticalBase = getVariable('verticalBase')(props);
-		const uiSmallSize = getVariable('uiSmallSize')(props);
-		const uiSmallLineHeight = getVariable('uiSmallLineHeight')(props);
-		const uiRegularSize = getVariable('uiRegularSize')(props);
-		const uiRegularLineHeight = getVariable('uiRegularLineHeight')(props);
+	// If props.inset is true, we will remove half the vertical padding
+	// We use this for elements like buttons and search forms that do not
+	// cover the entire height of the link bar
+	const insetFactor = inset ? 1/2 : 1;
 
-		// If props.inset is true, we will remove half the vertical padding
-		// We use this for elements like buttons and search forms that do not
-		// cover the entire height of the link bar
-		const insetFactor = inset ? 1/2 : 1;
+	let verticalPadding;
+	let horizontalPadding;
+	let smVerticalPadding;
 
-		let verticalPadding;
-		let horizontalPadding;
-		let smVerticalPadding;
+	if (size === 'xsmall') {
+		verticalPadding = '0';
+		smVerticalPadding = verticalPadding;
+		horizontalPadding = `calc(1/2 * ${horizontalBase})`;
+	} else if (size === 'small') {
+		verticalPadding = `calc(${insetFactor} * 1/2 * (3/2*${verticalBase} - ${uiSmallLineHeight}) )`;
+		smVerticalPadding = `calc(${insetFactor} * 1/2 * ( 3/2*${verticalBase} - ${uiSmallLineHeight}) )`;
+		horizontalPadding = `calc(1/2 * ${horizontalBase})`;
+	} else if (size === 'large') {
+		verticalPadding = `calc(${insetFactor} * 1/2 * (4/2*${verticalBase} - ${uiRegularLineHeight}) )`;
+		smVerticalPadding = `calc(${insetFactor} * 1/2 * ( 5/2*${verticalBase} - ${uiRegularLineHeight}) )`;
+		horizontalPadding = horizontalBase;
+	} else {
+		// size === medium
+		verticalPadding = `calc(${insetFactor} * 1/2 * (3/2*${verticalBase} - ${uiRegularLineHeight}) )`;
+		smVerticalPadding =	`calc(${insetFactor} * 1/2 * ( 2*${verticalBase} - ${uiRegularLineHeight}) )`;
+		horizontalPadding = horizontalBase;
+	}
 
-		if (size === 'xsmall') {
-			verticalPadding = '0';
-			smVerticalPadding = verticalPadding;
-			horizontalPadding = `calc(1/2 * ${horizontalBase})`;
-		} else if (size === 'small') {
-			verticalPadding = `calc(${insetFactor} * 1/2 * (3/2*${verticalBase} - ${uiSmallLineHeight}) )`;
-			smVerticalPadding = `calc(${insetFactor} * 1/2 * ( 3/2*${verticalBase} - ${uiSmallLineHeight}) )`;
-			horizontalPadding = `calc(1/2 * ${horizontalBase})`;
-		} else if (size === 'large') {
-			verticalPadding = `calc(${insetFactor} * 1/2 * (4/2*${verticalBase} - ${uiRegularLineHeight}) )`;
-			smVerticalPadding = `calc(${insetFactor} * 1/2 * ( 5/2*${verticalBase} - ${uiRegularLineHeight}) )`;
-			horizontalPadding = horizontalBase;
-		} else {
-			// size === medium
-			verticalPadding = `calc(${insetFactor} * 1/2 * (3/2*${verticalBase} - ${uiRegularLineHeight}) )`;
-			smVerticalPadding =	`calc(${insetFactor} * 1/2 * ( 2*${verticalBase} - ${uiRegularLineHeight}) )`;
-			horizontalPadding = horizontalBase;
+	const fontSize = ['xsmall', 'small'].includes(props.size)
+		? uiSmallSize
+		: uiRegularSize;
+	const lineHeight = ['xsmall', 'small'].includes(props.size)
+		? uiSmallLineHeight
+		: uiRegularLineHeight;
+
+	const smBreakpoint = props.theme.flexboxgrid.breakpoints.sm;
+
+	return css`
+		font-size: ${fontSize};
+		line-height: ${lineHeight};
+		padding: ${verticalPadding} ${horizontalPadding};
+		margin: ${inset ? `${verticalPadding} 0` : 0};
+
+		@media screen and (min-width: ${smBreakpoint}em) {
+			padding: ${smVerticalPadding} ${horizontalPadding};
+			margin: ${inset ? `${smVerticalPadding} 0` : 0};
+		}
+	`;
+};
+
+const allcaps = props => props.ALLCAPS && css`
+	text-transform: uppercase;
+	letter-spacing: .1rem;
+`;
+
+/* This is the underline feature */
+const underlineFeature = (props) => {
+	const {
+		isActive,
+		useUnderline,
+		size,
+		skin,
+		theme: { colors },
+	} = props;
+	const horizontalBase = getVariable('horizontalBase')(props);
+	const sizeFactor = ['xsmall', 'small'].includes(size) ? 1/2 : 1;
+
+	const width = (
+		useUnderline
+			? `calc( 100% - 2 * ${sizeFactor} * ${horizontalBase} )`
+			: '0'
+	);
+
+	const background = colors[colors.skinColors[skin]] || colors.primary;
+
+	return css`
+		&::after {
+			width: ${isActive ? width :'0'};
+			display: block;
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			height: .1rem;
+			margin: 0 calc(${sizeFactor} * ${horizontalBase});
+			background: ${background};
+			content: '';
+			transition: width .2s ease-in-out;
 		}
 
-		const fontSize = ['xsmall', 'small'].includes(props.size)
-			? uiSmallSize
-			: uiRegularSize;
-		const lineHeight = ['xsmall', 'small'].includes(props.size)
-			? uiSmallLineHeight
-			: uiRegularLineHeight;
+		&:hover::after {
+			width: ${width};
+		}
+	`;
+};
 
-		return css`
-			font-size: ${fontSize};
-			line-height: ${lineHeight};
-			padding: ${verticalPadding} ${horizontalPadding};
-			margin: ${inset ? `${verticalPadding} 0` : 0};
-			@media screen and (min-width: ${props.theme.flexboxgrid.breakpoints.sm}em) {
-				padding: ${smVerticalPadding} ${horizontalPadding};
-				margin: ${inset ? `${smVerticalPadding} 0` : 0};
+/*
+ * As opposed to other links,
+ * blocklinks will not have
+ * line-height or horizontal padding.
+ * Often used for logos or other images.
+ */
+const blocklinks = (props) => {
+	const { isBlockLink } = props;
+	if (!isBlockLink) {
+		return '';
+	}
+
+	return css`
+		&& {
+			line-height: 0;
+			padding: 0;
+		}
+	`;
+};
+
+const backgroundColor = (props) => {
+	const {
+		isActive, activeBackground, activeBackgroundColor, backgroundColor,
+	} = props;
+	let background='';
+
+	if (isActive) {
+		if (activeBackground && !activeBackgroundColor) {
+			log('The `activeBackground` prop has been deprecated. Use the `activeBackgroundColor instead.'
+			+ ' It expects a color name from your theme instead of an actual css color string.`');
+		}
+
+		if (!activeBackgroundColor) {
+			// Deprecated actual css color
+			background = activeBackground;
+		} else {
+			// Let's use the color palette / getColor when we can
+			background = getColor(activeBackgroundColor)(props);
+		}
+	} else {
+		background = getColor(backgroundColor)(props);
+	}
+
+	const hoverBackground = props.activeBackgroundColor
+		? getColor(props.activeBackgroundColor)(props)
+		: props.activeBackground;
+	const focusBackground = getFocusBackground(props);
+
+	return css`
+		background: ${background};
+
+		&, &:visited {
+			:hover {
+				background: ${hoverBackground};
 			}
-		`;
-	}}
+		}
+
+		&:focus {
+			background: ${focusBackground};
+			box-shadow: none;
+
+			&:hover {
+				background: ${focusBackground};
+			}
+		}
+
+	`;
+};
+
+export const LinkBarElementBase = styled.a`
+	display: inline-block;
 
 	border: 0;
 	${props => props.rounded && 'border-radius: .3rem;'}
@@ -96,112 +230,17 @@ export const LinkBarElementBase = styled.a`
 	font-family: ${getVariable('headingsFont')};
 	font-weight: ${props => (props.isActive ? '600' : '400')};
 	transition: padding .2s;
-	background: ${(props) => {
-		if (props.isActive) {
-			if (props.activeBackground && !props.activeBackgroundColor) {
-				log('The `activeBackground` prop has been deprecated. Use the `activeBackgroundColor instead.'
-				+ ' It expects a color name from your theme instead of an actual css color string.`');
-			}
-
-			if (!props.activeBackgroundColor) {
-				// Deprecated actual css color
-				return props.activeBackground;
-			}
-
-			// Let's use the color palette / getColor when we can
-			return getColor(props.activeBackgroundColor)(props);
-		}
-		return getColor(props.backgroundColor)(props);
-	}};
-	${props => props.ALLCAPS && css`
-		text-transform: uppercase;
-		letter-spacing: .1rem;
-	`}
-
-	/*
-	 * As opposed to other links,
-	 * blocklinks will not have
-	 * line-height or horizontal padding
-	 */
-	${(props) => {
-		const { isBlockLink } = props;
-		if (!isBlockLink) {
-			return '';
-		}
-
-		return css`
-			&& {
-				line-height: 0;
-				padding: 0;
-			}
-		`;
-	}}
 
 	:hover {
 		text-decoration: none;
 	}
 
-	&, &:visited {
-		color: ${getTextColor};
-		:hover {
-			background:
-				${
-	props => (props.activeBackgroundColor
-		? getColor(props.activeBackgroundColor)(props)
-		: props.activeBackground)
-};
-			color: ${props => getColor(props.activeTextColor || props.textColor)};
-		}
-	}
-
-	&:focus {
-		background: ${getFocusBackground};
-		box-shadow: none;
-
-		&:hover {
-			background: ${getFocusBackground};
-		}
-	}
-
-	/* This is the underline feature */
-	${(props) => {
-		const {
-			isActive,
-			useUnderline,
-			size,
-			skin,
-			theme: { colors },
-		} = props;
-		const horizontalBase = getVariable('horizontalBase')(props);
-		const sizeFactor = ['xsmall', 'small'].includes(size) ? 1/2 : 1;
-
-		const width = (
-			useUnderline
-				? `calc( 100% - 2 * ${sizeFactor} * ${horizontalBase} )`
-				: '0'
-		);
-
-		const background = colors[colors.skinColors[skin]] || colors.primary;
-
-		return css`
-			&::after {
-				width: ${isActive ? width :'0'};
-				display: block;
-				position: absolute;
-				bottom: 0;
-				left: 0;
-				height: .1rem;
-				margin: 0 calc(${sizeFactor} * ${horizontalBase});
-				background: ${background};
-				content: '';
-				transition: width .2s ease-in-out;
-			}
-
-			&:hover::after {
-				width: ${width};
-			}
-		`;
-	}}
+	${linkBarElementSizes}
+	${textColor}
+	${backgroundColor};
+	${allcaps}
+	${blocklinks}
+	${underlineFeature}
 `;
 LinkBarElementBase.propTypes = {
 	/** Display text in capital letters */
