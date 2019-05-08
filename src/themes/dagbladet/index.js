5ad3d8aeb25c28abf4758f99d675f74719949d92
Morgan <morgan.bro@dagbladet.no>
2019-05-08 14:25:58 +0200

    Changed lineheight for uiSmallSize

--- src/themes/dagbladet/index.js
+++ src/themes/dagbladet/index.js
@@ -11,7 +11,7 @@ const variables = {
 	uiRegularSize: '1.6rem',
 	uiRegularLineHeight: '2.4rem',
 	uiSmallSize: '1.4rem',
-	uiSmallLineHeight: '1.9rem',
+	uiSmallLineHeight: '1.6rem',
 }
 
 const global = `
