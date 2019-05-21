9b61ba50d0cabb2d46ea3fce7b9b0ced80950ed7
misund <thomas@hemmeligadresse.com>
2019-05-21 12:51:09 +0200

    refactor(Gears): capitalize react component filename

--- src/atoms/SvgIcon/index.js
+++ src/atoms/SvgIcon/index.js
@@ -39,7 +39,7 @@ const icons = {
 	'dagbladetVideo': () => import('./DagbladetVideo'),
 	'backNav': () => import('./BackNav'),
 	'not-found': () => import('./NotFound'),
-	'gears': () => import('./gears'),
+	'gears': () => import('./Gears'),
 	'default': () => <div />,
 };
 
