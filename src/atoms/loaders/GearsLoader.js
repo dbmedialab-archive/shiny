a24826bca114602e4b13199fba7ae10573254153
misund <thomas@hemmeligadresse.com>
2019-05-21 12:41:56 +0200

    refactor(Gears, GearsLoader): Remove two DOM node levels

--- src/atoms/loaders/GearsLoader.js
+++ src/atoms/loaders/GearsLoader.js
@@ -2,17 +2,17 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { SvgIcon } from '../SvgIcon';
 
-const StyledLoader = styled.div`
+const SpinningIcon = styled(SvgIcon)`
 	.gears-icon-large {
 	  	transform-origin: 34% 34%;
 	  	animation: rotate 2.0s linear infinite
 	}
-	
+
 	.gears-icon-small {
 	   transform-origin: 74% 74%;
 	   animation: reverseRotate 2.0s linear infinite;
 	}
-	
+
 	@keyframes rotate {
 		0% {
 			transform: rotate(0deg);
@@ -21,7 +21,7 @@ const StyledLoader = styled.div`
 		  transform: rotate(360deg);
 	   }
 	}
-	
+
 	@keyframes reverseRotate {
 	   0% {
 		  transform: rotate(360deg);
@@ -33,9 +33,11 @@ const StyledLoader = styled.div`
 `;
 
 const GearsLoader = props => (
-	<StyledLoader>
-		<SvgIcon color="primary" name="gears" size={10} {...props} />
-	</StyledLoader>
+	<SpinningIcon name="gears" {...props} />
 );
+GearsLoader.defaultProps = {
+	color: 'primary',
+	size: 10,
+};
 
 export { GearsLoader };
