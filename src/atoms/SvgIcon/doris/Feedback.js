ebb0f5d1df721970618caf500339b9a6f6b0ca0b
misund <thomas@hemmeligadresse.com>
2019-05-27 17:22:35 +0200

    fix(doris icons): tweak view boxes

--- src/atoms/SvgIcon/doris/Feedback.js
+++ src/atoms/SvgIcon/doris/Feedback.js
@@ -3,14 +3,12 @@ import React from 'react';
 import Svg from './Svg';
 
 const Feedback = props => (
-	<Svg viewBox="0 0 35 32" {...props}>
-		<g transform="translate(-1.000000, -2.000000)">
-			<rect fill="none" stroke="none" x="0" y="0" width="36" height="36" />
-			<polyline strokeLinecap="round" strokeLinejoin="round" points="11 11 1.5 11 1.5 27.6 1.5 33.1 7.8 27.6 25.9 27.6 25.9 19.1 25.9 19.1" />
-			<polygon strokeLinecap="round" strokeLinejoin="round" points="11 2.5 11 19.1 29.1 19.1 35.4 24.6 35.4 19.1 35.4 2.5" />
-			<path d="M15.4,9.2 L29.9,9.2" strokeLinecap="round" strokeLinejoin="round" />
-			<path d="M15.4,12.8 L29.9,12.8" strokeLinecap="round" strokeLinejoin="round" />
-		</g>
+	<Svg viewBox="0 0 36 35" {...props}>
+		<rect fill="none" stroke="none" x="0" y="0" width="36" height="36" />
+		<polyline strokeLinecap="round" strokeLinejoin="round" points="11 11 1.5 11 1.5 27.6 1.5 33.1 7.8 27.6 25.9 27.6 25.9 19.1 25.9 19.1" />
+		<polygon strokeLinecap="round" strokeLinejoin="round" points="11 2.5 11 19.1 29.1 19.1 35.4 24.6 35.4 19.1 35.4 2.5" />
+		<path d="M15.4,9.2 L29.9,9.2" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M15.4,12.8 L29.9,12.8" strokeLinecap="round" strokeLinejoin="round" />
 	</Svg>
 );
 
--- src/atoms/SvgIcon/doris/Search.js
+++ src/atoms/SvgIcon/doris/Search.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Svg from './Svg';
 
 const Search = props => (
-	<Svg viewBox="-2 -2 26 26" {...props}>
+	<Svg viewBox="-2 -2 32 32" {...props}>
 		<circle cx="10.2" cy="10.2" r="9.7" />
 		<line x1="24.3" y1="23.7" x2="17.3" y2="17.1" />
 	</Svg>
