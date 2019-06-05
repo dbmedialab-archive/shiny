1c63e3ab1ed89eacd95d5262c839cbb9e07e8486
misund <thomas@hemmeligadresse.com>
2019-06-05 13:10:23 +0200

    fix(doris icons): remove superfluous elements

--- src/atoms/SvgIcon/doris/HorizontalEllipse.js
+++ src/atoms/SvgIcon/doris/HorizontalEllipse.js
@@ -4,9 +4,9 @@ import Svg from './Svg';
 
 const HorizontalEllipse = props => (
 	<Svg viewBox="0 0 36 9" fillRule="nonzero" {...props}>
-		<circle id="Oval" cx="4.5" cy="4.5" r="4.5" />
-		<circle id="Oval" cx="17.5" cy="4.5" r="4.5" />
-		<circle id="Oval" cx="31.5" cy="4.5" r="4.5" />
+		<circle cx="4.5" cy="4.5" r="4.5" />
+		<circle cx="17.5" cy="4.5" r="4.5" />
+		<circle cx="31.5" cy="4.5" r="4.5" />
 	</Svg>
 );
 
