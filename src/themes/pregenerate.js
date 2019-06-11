8ac59e0927bd9ed003a5d43b94dc05889cd5f5a3
jimoe <jim@sol.no>
2019-06-11 10:38:02 +0200

    Reinserting elbil24Dark theme (#1237)
    * Reinserting elbil24Dark theme
    
    * One more file

--- src/themes/pregenerate.js
+++ src/themes/pregenerate.js
@@ -3,10 +3,10 @@ import path from 'path';
 import { themes } from './index';
 
 const themeNames = Object.keys(themes).filter(
-	theme => !['defaultTheme', 'lightTheme', 'dagbladetDark', 'elbil24'].includes(theme)
+	theme => !['defaultTheme', 'lightTheme', 'dagbladetDark', 'elbil24Dark'].includes(theme)
 );
 
-const themeFolderNames = [...themeNames, 'default-theme', 'light-theme', 'dagbladet-dark', 'elbil24'];
+const themeFolderNames = [...themeNames, 'default-theme', 'light-theme', 'dagbladet-dark', 'elbil24-dark'];
 
 const prefix = 'module.exports = ';
 const themeLibDir = path.join(__dirname, '..', '..', 'lib', 'themes');
@@ -29,8 +29,8 @@ function getThemeName(folder) {
 	if (folder === 'dagbladet-dark') {
 		return 'dagbladetDark';
 	}
-	if (folder === 'elbil24') {
-		return 'elbil24';
+	if (folder === 'elbil24-dark') {
+		return 'elbil24Dark';
 	}
 	return folder;
 }
