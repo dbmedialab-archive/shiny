5ef46ed316942a6551dd2ded860ba6838eada28c
misund <thomas@hemmeligadresse.com>
2019-06-19 17:36:56 +0200

    feature(SeHerLogo): Add a pride variant

--- stories/logos/SeHerStory.js
+++ stories/logos/SeHerStory.js
@@ -1,38 +1,26 @@
 import React from 'react';
-import { DemoContainer } from '../storybook-components';
+import { withInfo } from '@storybook/addon-info';
 
 import {
-	Heading, HugeHeading,
-	Paragraph,
+	Row, Col,
 	SeHerLogo,
 } from '../../src';
 
-import { Code } from '../../src/atoms/Code';
-
 const SeHerStory = () => (
 	<section>
-		<HugeHeading>SeHerLogo</HugeHeading>
-		<Paragraph>This svg beauty grabs all the space it can get, before displaying
-			the SeHer logo in all its outwards exploding glory.
-		</Paragraph>
-
-		<Heading>Demo</Heading>
-		<DemoContainer>
-			<SeHerLogo />
-		</DemoContainer>
-
-		<Heading>Props</Heading>
-		<Paragraph>There are none.</Paragraph>
-
-		<Heading>Usage</Heading>
-		<Code language="jsx">
-			{`
-import { SeHerLogo } from '@aller/shiny';
-
-<SeHerLogo />
-			`}
-		</Code>
+		<Row>
+			<Col xs><SeHerLogo variant="default" /></Col>
+			<Col xs><SeHerLogo variant="pride" /></Col>
+		</Row>
 	</section>
 );
 
-export { SeHerStory };
+export default withInfo({
+	text: `
+<p>This svg beauty grabs all the space it can get, before displaying the SeHer
+	logo in all its outwards exploding glory.</p>
+
+<p>Variants: To display a certain variant, either register the dates for the
+	variant in the component, or pass the 'variant' prop.</p>
+	`,
+})(SeHerStory);
