84382291b2667b10a5d53932ba3c45248de9b1cd
Just Thomas Hiorth Misund <thomas@hemmeligadresse.com>
2019-07-10 16:50:26 +0200

    feature(FullscreenAd): support non-fullscreen ads in fullscreen slots (#1300)
    * feature(FullscreenAd): support non-fullscreen ads in fullscreen slots
    
    * test(FullscreenAd): add more sizes to the fullscreen ads story
    
    * v0.42.0-0

--- src/molecules/FullscreenAd.js
+++ src/molecules/FullscreenAd.js
@@ -16,58 +16,84 @@ if (
 }
 
 const StyledReserveSpacer = styled.div`
-	height: 100vh;
-	width: 100%;
+	height: ${p => (p.isBannerAdInFullscreenSlot ? p.height: '100vh')};
+	width: ${p => (p.isBannerAdInFullscreenSlot ? p.width: '100%')};
+	${p => p.isBannerAdInFullscreenSlot && css`margin: auto;`}
 	display: ${props => (props.shouldHide ? 'none' : 'block')};
 `;
 
-const StyledWrapper = styled.div`
+const AdUnitWrapper = styled.div`
 	position: absolute;
 	top: 0;
-	left: 0;
-  height: 100vh;
-  width: 100%;
+	${p => !p.isBannerAdInFullscreenSlot && css`
+		left: 0;
+	  height: 100vh;
+	  width: 100%;
+	`}
 	overflow: hidden;
   margin: 0;
   padding: 0;
   display: ${props => (props.shouldHide ? 'none' : 'block')};
 	z-index: 10;
+	text-align: center;
+
 	${props => !props.isIE11 && css`
 		clip: rect(2.4rem, 100vw, 100vh, 0);
 	`}
 
   & > div {
 		z-index: 1;
-    position: ${props => (props.isIE11 ? 'absolute' : 'fixed')};
     top: ${props => (props.isIE11 ? '2.4rem' : '0')};
     right: 0;
 		left: 0;
 		bottom: 0;
-		width: 100% !important;
-		height: 100vh !important;
 		transform: translate3d(0px, 0px, 0px);
+		${p => !p.isBannerAdInFullscreenSlot && css`
+			position: ${props => (props.isIE11 ? 'absolute' : 'fixed')};
+			width: 100% !important;
+			height: 100vh !important;
+		`}
   }
 
   & iframe {
-    width: 100% !important;
-    height: 100vh !important;
+		text-align: center;
+		${p => !p.isBannerAdInFullscreenSlot && css`
+	    width: 100% !important;
+	  	height: 100% !important;
+		`}
   }
 `;
 
 
 const FullscreenAd = forwardRef((props, ref) => {
-	const { shouldHide } = props;
+	const {
+		shouldHide, isBannerAdInFullscreenSlot, width, height,
+	} = props;
 
 	return (
-		<StyledReserveSpacer shouldHide={shouldHide} ref={ref}>
+		<StyledReserveSpacer
+			shouldHide={shouldHide}
+			ref={ref}
+			isBannerAdInFullscreenSlot={isBannerAdInFullscreenSlot}
+			width={width}
+			height={height}
+		>
 			<AdWrapper width="100%" />
-			<StyledWrapper {...props}  />
+			<AdUnitWrapper {...props} isBannerAdInFullscreenSlot={isBannerAdInFullscreenSlot} />
 		</StyledReserveSpacer>
 	);
 });
 
 FullscreenAd.propTypes = {
 	shouldHide: PropTypes.bool.isRequired,
+	isBannerAdInFullscreenSlot: PropTypes.bool,
+	width: PropTypes.string,
+	height: PropTypes.string,
+};
+FullscreenAd.defaultProps = {
+	isBannerAdInFullscreenSlot: false,
+	width: '100%',
+	height: '100vh',
 };
 
 export { FullscreenAd };
