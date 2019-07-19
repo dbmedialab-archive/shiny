97d8eaaa215a1cbe50ab9c0175147b183938b431
Just Thomas Hiorth Misund <thomas@hemmeligadresse.com>
2019-07-19 13:41:49 +0200

    fix(LinkBarSearchField): switch to dorris icons (#1319)
    * fix(LinkBarSearchField): switch to dorris icons
    
    * docs(link-bar-element-sizes): move documentation along with the function

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,4 @@ export { getVariable } from './get-variable';
 export { unshadeColorString } from './unshade-color-string';
 export { isBright, isDark } from './luminance';
 export { calculateTextColorFromName, calculateTextColorFromTheme } from './calculate-text-color';
+export { linkBarElementSizes } from './link-bar-element-sizes';
