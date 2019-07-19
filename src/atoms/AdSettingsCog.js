3d83895631a4cd23d1dec4e34eca73805a536e6a
misund <thomas@hemmeligadresse.com>
2019-07-19 15:33:57 +0200

    fix(AdSettingsCog): use filled icon

--- src/atoms/AdSettingsCog.js
+++ src/atoms/AdSettingsCog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { getVariable } from '../utils';
+import { getColor, getVariable } from '../utils';
 import { SvgIcon } from './SvgIcon';
 
 const StyledCog = styled.div`
@@ -8,7 +8,9 @@ const StyledCog = styled.div`
 	left: 0;
 	top: ${getVariable('verticalBase')};
 	padding: calc(1/2 * ${getVariable('horizontalBase')});
-	background-color: rgba(255,255,255,0.5);
+	background-color: ${getColor('white')};
+	opacity: 0.5;
+	filter: alpha(opacity=50);
 	line-height: 0;
 	border-bottom-right-radius: 50%;
 	cursor: pointer;
@@ -16,7 +18,7 @@ const StyledCog = styled.div`
 
 const AdSettingsCog = props => (
 	<StyledCog {...props}>
-		<SvgIcon name="settings" set="dorris" color="type" size={1.6} />
+		<SvgIcon name="settings" set="dorris" color="type" size={1.6} fill="currentColor" />
 	</StyledCog>
 );
 
