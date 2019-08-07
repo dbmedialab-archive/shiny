e6e07f8fed5fcbd465dd9ff035b9217f4fbf5540
Nataly Kulychyk <nkul@ciklum.com>
2019-08-07 13:22:35 +0300

    add dynamic import

--- src/atoms/SvgIcon/index.js
+++ src/atoms/SvgIcon/index.js
@@ -1,50 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import dorrisIcons from './dorris';
+import defaultIconSet from './default';
 
 import { SvgIconWrapper } from './SvgIconWrapper';
 
-import defaultIconSet from './default';
-import dorris from './dorris';
-
 const iconSets = {
 	default: defaultIconSet,
-	dorris,
+	dorris: dorrisIcons,
 };
 
 class SvgIcon extends React.PureComponent {
+
 	constructor(props) {
 		super(props);
-		this.state = { module: () => null };
+		this.state = { LazyIcon: () => null };
 	}
 
-	async componentWillMount() {
-		try {
-			console.log('will mount start')
-			const { name, set } = this.props;
-
-			const icons = iconSets[set];
-
-			const resolveIcon = icons[name] || icons.default();
-			const response = await resolveIcon();
-			console.log(response.default)
-			console.log('will mount end')
-			return Promise.resolve(this.setState({ module: response.default }));
-		} catch (error) {
-			return error;
-		}
+	componentWillMount() {
+		const { set, name } = this.props;
+		const setName = set || 'default';
+		const iconName = name || 'default';
+		const LazyIcon = iconSets[setName][iconName];
+		return this.setState({ LazyIcon });
 	}
 
 	render() {
-		const { module: Icon } = this.state;
-		console.log('render')
+		const { LazyIcon } = this.state;
 		const {
 			size, className, ...rest
 		} = this.props;
-		console.log(Icon({ ...rest }))
 		return (
 			<SvgIconWrapper size={size} size-sm={rest['size-sm']} className={className}>
-				<Icon {...rest} />
+				<LazyIcon {...rest} />
 			</SvgIconWrapper>
 		);
 	}
